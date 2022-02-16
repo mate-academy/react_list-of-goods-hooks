9927514ad0c1a78b6b7656935fb344a10e3ebddf
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-02-16 17:04:14 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+import { Button } from './components/Button';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,46 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [state, setState] = useState(false);
+  const [goods, setGoods] = useState(goodsFromServer);
+
+  const togling = () => {
+    setState((bool) => !bool);
+  };
+
+  const reversing = () => {
+    setGoods(() => [...goods].reverse());
+  };
+
+  const sortAlphabet = () => {
+    setGoods(() => [...goods].sort((a, b) => a.localeCompare(b)));
+  };
+
+  const sortByLength = () => {
+    setGoods(() => [...goods].sort((a, b) => a.length - b.length));
+  };
+
+  const reseting = () => {
+    setGoods(() => [...goodsFromServer]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      <Button clicked={togling} name="START" />
+      <Button clicked={reversing} name="Reverse" />
+      <Button clicked={sortAlphabet} name="Sort alphabetically" />
+      <Button clicked={reseting} name="Reset" />
+      <Button clicked={sortByLength} name="Sort by length" />
+
+      <ul>
+        {state && goods.map((good) => (
+          <li key={good}>{good}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
