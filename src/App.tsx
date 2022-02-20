e04b68162fbe71ed6bb2866777a4219448a576b9
Mykhailo Ianush <yanushmisha@gmail.com>
2022-02-20 20:21:27 +0200

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,104 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, setVisible] = useState(false);
+  const [sortBy, setSortBy] = useState('');
+  const [isReverse, reverse] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(1);
+
+  const selectValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const visibleGoods = [...goodsFromServer].filter(value => value.length >= selectedValue);
+
+  switch (sortBy) {
+    case 'order':
+      visibleGoods.sort();
+      break;
+    case 'length':
+      visibleGoods.sort((a, b) => a.length - b.length);
+      break;
+    default:
+      break;
+  }
+
+  if (isReverse) {
+    visibleGoods.reverse();
+  }
+
+  const reverseFunc = () => {
+    reverse(current => !current);
+  };
+
+  const reset = () => {
+    setSortBy('');
+    setSelectedValue(1);
+    reverse(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {!isVisible
+          && (
+            <button
+              type="button"
+              className="App__button"
+              onClick={() => setVisible(true)}
+            >
+              Start
+            </button>
+          )}
+      {isVisible && (
+        <>
+          <ul>
+            {visibleGoods.map(good => (
+              <li key={good}>{good}</li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className={classNames('App__button', { App__active: isReverse })}
+            onClick={reverseFunc}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            className={classNames('App__button', { App__active: sortBy === 'order' })}
+            onClick={() => setSortBy('order')}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            className="App__button"
+            onClick={reset}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            className={classNames('App__button', { App__active: sortBy === 'length' })}
+            onClick={() => setSortBy('length')}
+          >
+            Sort by length
+          </button>
+          <select
+            onChange={
+              (event) => setSelectedValue(+event.target.value)
+            }
+            className="App__button"
+            value={selectedValue}
+          >
+            {selectValues.map(value => (
+              <option value={value}>{value}</option>
+            ))}
+          </select>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
