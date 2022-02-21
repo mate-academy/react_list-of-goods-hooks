ad4d61d3e107c34bb35bed3f77aaf169e39d0440
AndryKK <korbutiaktso@gmail.com>
2022-02-21 10:21:56 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,86 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [initialOrder] = useState([...goodsFromServer]);
+  const [isStarted, setIsStarted] = useState(false);
+  const [actualOrder, setActualOrder] = useState([...goodsFromServer]);
+
+  const start = () => {
+    setIsStarted(true);
+  };
+
+  const sortAlphabetically = () => {
+    setActualOrder([...actualOrder].sort());
+  };
+
+  const reverseItem = () => {
+    setActualOrder([...actualOrder].reverse());
+  };
+
+  const reset = () => {
+    setActualOrder([...initialOrder]);
+  };
+
+  const lengthSort = () => {
+    setActualOrder([...actualOrder].sort((a, b) => a.length - b.length));
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {isStarted && (
+        <ul>
+          {actualOrder.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+      )}
+      {!isStarted && (
+        <button
+          type="button"
+          className="button"
+          onClick={start}
+        >
+          Start
+        </button>
+      )}
+      {isStarted && (
+        <div>
+          <button
+            type="button"
+            className="button"
+            onClick={reverseItem}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={reset}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={lengthSort}
+          >
+            Sort by length
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
