6b390c3557ca66b8691fc9708207237597d392fe
Alex <asultan4ik@gmail.com>
2022-02-21 13:16:23 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,52 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [start, setStart] = useState(false);
+  const [goods, setGoods] = useState(goodsFromServer);
+
+  return (
+    <div className="App">
+      <button
+        type="button"
+        hidden={start}
+        onClick={() => setStart(current => !current)}
+      >
+        Start
+      </button>
+      <div hidden={!start}>
+        <ul>
+          {goods.map(good => (
+            <li>{good}</li>
+          ))}
+          <button
+            type="button"
+            onClick={() => setGoods(current => [...current].reverse())}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            onClick={() => setGoods(current => [...current].sort((a, b) => a.localeCompare(b)))}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            onClick={() => setGoods([...goodsFromServer])}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            onClick={() => setGoods(current => [...current].sort((a, b) => a.length - b.length))}
+          >
+            Sort by length
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
