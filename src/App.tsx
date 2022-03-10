18f26119f03ef76c5e0789c1886ac38876247efc
Tykhon Shuliak <tiandzh2013@gmail.com>
2022-03-10 15:27:35 +0200

    reSolution

--- src/App.tsx
+++ src/App.tsx
@@ -18,33 +18,27 @@ const goodsFromServer: string[] = [
 const App: React.FC = () => {
   const [confirmBtn, setState] = useState(true);
   const [selectedOption, setSelect] = useState(1);
-  const [isreversed, setReverse] = useState(false);
-  const [sort, setSort] = useState('');
-  const serverGoodsList = [...goodsFromServer].filter(item => item.length >= selectedOption);
+  const [serverGoodsList, setServerLIst] = useState([...goodsFromServer]);
 
-  serverGoodsList.sort((a, b) => {
-    switch (sort) {
-      case 'a-z':
-        return a.localeCompare(b);
-      case 'length':
-        return a.length - b.length;
-      default:
-        return 0;
-    }
-  });
+  const sortByAlphabet = () => {
+    setServerLIst([...serverGoodsList].sort((a, b) => a.localeCompare(b)));
+  };
+
+  const sortByLength = () => {
+    setServerLIst([...serverGoodsList].sort((a, b) => a.length - b.length));
+  };
 
   const handleSelector = (e: string) => {
     setSelect(+e);
   };
 
-  if (isreversed) {
-    serverGoodsList.reverse();
-  }
+  const reversed = () => {
+    setServerLIst([...serverGoodsList].reverse());
+  };
 
   const resetList = () => {
+    setServerLIst([...goodsFromServer]);
     setSelect(1);
-    setSort('');
-    setReverse(false);
   };
 
   return (
@@ -54,14 +48,6 @@ const App: React.FC = () => {
         {selectedOption}
       </h1>
       <div className="button-section">
-        <input
-          type="range"
-          className="ranger-button"
-          value={selectedOption}
-          min={1}
-          max={10}
-          onChange={(e) => handleSelector(e.target.value)}
-        />
         {confirmBtn
           && (
             <button
@@ -74,53 +60,56 @@ const App: React.FC = () => {
           )}
         {!confirmBtn
           && (
-            <button
-              className="button"
-              type="button"
-              onClick={() => setReverse(prev => !prev)}
-            >
-              Reverse
-            </button>
+            <>
+              <button
+                className="button"
+                type="button"
+                onClick={reversed}
+              >
+                Reverse
+              </button>
+
+              <button
+                className="button"
+                type="button"
+                onClick={sortByAlphabet}
+              >
+                Sort A-Z
+              </button>
+              <button
+                className="button"
+                type="button"
+                onClick={resetList}
+              >
+                Reset
+              </button>
+              <button
+                className="button"
+                type="button"
+                onClick={sortByLength}
+              >
+                Sort Length
+              </button>
+              <select
+                className="button"
+                value={selectedOption}
+                onChange={(e) => handleSelector(e.target.value)}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </>
           )}
-        <button
-          className="button"
-          type="button"
-          onClick={() => setSort('a-z')}
-        >
-          Sort A-Z
-        </button>
-        <button
-          className="button"
-          type="button"
-          onClick={resetList}
-        >
-          Reset
-        </button>
-        <button
-          className="button"
-          type="button"
-          onClick={() => setSort('length')}
-        >
-          Sort Length
-        </button>
-        <select
-          className="button"
-          value={selectedOption}
-          onChange={(e) => handleSelector(e.target.value)}
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
       </div>
-      {!confirmBtn && <ListOfGoods goods={serverGoodsList} />}
+      {!confirmBtn && <ListOfGoods goods={serverGoodsList} select={selectedOption} />}
     </div>
   );
 };
--- src/ListOfGoods.tsx
+++ src/ListOfGoods.tsx
@@ -3,13 +3,14 @@ import './ListOfGoods.css';
 
 type Props = {
   goods: string[],
+  select: number,
 };
 
-export const ListOfGoods: React.FC<Props> = ({ goods }) => {
+export const ListOfGoods: React.FC<Props> = ({ goods, select }) => {
   return (
     <div className="list">
       <ul>
-        {goods.map(item => (
+        {goods.filter(item => item.length >= select).map(item => (
           <li key={item}>
             {item}
           </li>
