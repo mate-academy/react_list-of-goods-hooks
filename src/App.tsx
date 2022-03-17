20efbedaa07f6f22ead70b8365f21a6aa30a2055
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-17 14:33:56 +0200

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,102 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isReversed, setIsReversed] = useState(false);
+  const [sortBy, setSortBy] = useState('');
+
+  const show = () => {
+    setIsVisible(true);
+  };
+
+  const sortAlphabet = () => {
+    setSortBy('alphabet');
+  };
+
+  const sortLength = () => {
+    setSortBy('length');
+  };
+
+  const reversed = () => {
+    setIsReversed(!isReversed);
+  };
+
+  const reset = () => {
+    setIsReversed(false);
+    setSortBy('');
+  };
+
+  const visibleGoods = () => {
+    const showGoods = [...goodsFromServer];
+
+    switch (sortBy) {
+      case 'length':
+        showGoods.sort((a, b) => a.length - b.length);
+        break;
+
+      case 'alphabet':
+        showGoods.sort((el1, el2) => el1.localeCompare(el2));
+        break;
+
+      default:
+        break;
+    }
+
+    if (isReversed) {
+      showGoods.reverse();
+    }
+
+    return showGoods;
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      <ul className={
+        classNames(
+          'List',
+          { 'List--visible': !isVisible },
+        )
+      }
+      >
+        {visibleGoods().map((good) => <li key={good}>{good}</li>)}
+      </ul>
+      <div className="List__buttons">
+        <button
+          type="submit"
+          onClick={show}
+          className={isVisible ? 'button-hidden' : ''}
+        >
+          Start
+        </button>
+        <button
+          type="submit"
+          onClick={reversed}
+        >
+          Reverse
+        </button>
+        <button
+          type="submit"
+          onClick={sortAlphabet}
+        >
+          Sort by Alpabet
+        </button>
+        <button
+          type="submit"
+          onClick={sortLength}
+        >
+          Sort by Length
+        </button>
+        <button
+          type="button"
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
