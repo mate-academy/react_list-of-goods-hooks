ccd4d8a4961a0c93575f6783474dea2c1ea1d223
Yuriy Titov <yuranus@gmail.com>
2022-03-17 14:32:24 +0200

    feat: solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Button } from './components/Button';
+import { List } from './components/List';
+
+type OrderBy = 'alphabet' | 'length';
+
+const compareGoods = (good1: string, good2: string, orderBy: OrderBy) => {
+  switch (orderBy) {
+    case 'alphabet':
+      return good1.localeCompare(good2);
+    case 'length':
+      return good1.length - good2.length;
+
+    default:
+      return 0;
+  }
+};
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -15,27 +31,51 @@ const goodsFromServer: string[] = [
 ];
 
 const App: React.FC = () => {
+  const [list, setList] = useState(
+    goodsFromServer,
+  );
+
   const [clicked, setClicked] = useState(false);
+
+  const handlerToggleReverse = () => {
+    const nextList = [...list];
+
+    nextList.reverse();
+    setList(nextList);
+  };
+
+  const handlerSortByAlphabet = () => {
+    const nextList = [...list];
+
+    nextList.sort((a, b) => compareGoods(a, b, 'alphabet'));
+    setList(nextList);
+  };
+
+  const handlerSortByLength = () => {
+    const nextList = [...list];
+
+    nextList.sort((a, b) => compareGoods(a, b, 'length'));
+    setList(nextList);
+  };
+
+  const handlerReset = () => {
+    setList(goodsFromServer);
+  };
 
   return (
     <div className="App">
       <h1>Goods</h1>
-      {!clicked ? (
-        <button
-          type="button"
-          onClick={() => setClicked(true)}
-        >
-          Start
-        </button>
-      ) : (
-        <ul>
-          {goodsFromServer.map(item => (
-            <li key={item}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
+      {!clicked
+        ? <Button txt="Start" handleClick={() => setClicked(true)} />
+        : (
+          <>
+            <List list={list} />
+            <Button txt="Reverse" handleClick={handlerToggleReverse} />
+            <Button txt="Sort alphabetically" handleClick={handlerSortByAlphabet} />
+            <Button txt="Sort by length" handleClick={handlerSortByLength} />
+            <Button txt="Reset" handleClick={handlerReset} />
+          </>
+        )}
     </div>
   );
 };
