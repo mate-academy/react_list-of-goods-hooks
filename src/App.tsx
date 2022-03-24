0d24281d558eb49ab8a98b0aaa0bef33e5b8a1ed
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-03-24 15:55:55 +0200

    Create react-list-of-goods-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import classNames from 'classnames';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,120 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [start, setStart] = useState(false);
+  const [revers, setRevers] = useState(false);
+  const [typeSort, setTypeSort] = useState('none');
+  const [fildSize, setFildSize] = useState(goodsFromServer.length);
+
+  function reset() {
+    setRevers(false);
+    setTypeSort('none');
+    setFildSize(goodsFromServer.length);
+  }
+
+  return (
+    <div className="App bg-info">
+      <div className="App-wrap">
+        {!start && (
+          <button
+            className="btn bg-primary btn-my"
+            type="button"
+            onClick={() => setStart(true)}
+          >
+            Start
+          </button>
+        )}
+        {start && (
+          <div>
+            <h1 className="card-title">Goods</h1>
+            <div className="App-goods-wrap">
+              <GoodsList
+                data={goodsFromServer}
+                revers={revers}
+                typeSort={typeSort}
+                fildSize={fildSize}
+              />
+            </div>
+
+            <p>{`List length ${fildSize}`}</p>
+
+            <div className="btn-wrap">
+
+              <button
+                className={classNames(
+                  'btn',
+                  { 'btn-success': revers },
+                )}
+                type="button"
+                onClick={() => {
+                  setRevers(!revers);
+                }}
+              >
+                Is reverse
+              </button>
+
+              <button
+                className={classNames(
+                  'btn',
+                  { 'btn-success': typeSort === 'alfabet' },
+                )}
+                type="button"
+                onClick={() => {
+                  setTypeSort('alfabet');
+                }}
+              >
+                Alfabet
+              </button>
+
+              <button
+                className={classNames(
+                  'btn',
+                  { 'btn-success': typeSort === 'length' },
+                )}
+                type="button"
+                onClick={() => {
+                  setTypeSort('length');
+                }}
+              >
+                Length
+              </button>
+
+              <button
+                className="btn bg-primary"
+                type="button"
+                onClick={() => {
+                  reset();
+                }}
+              >
+                Reset
+              </button>
+            </div>
+
+            <select
+              className="form-select"
+              value={fildSize}
+              onChange={(event) => {
+                setFildSize(+event.target.value);
+              }}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
