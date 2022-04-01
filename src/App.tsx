ade7586552b3dc6ffab38d20a6f1afba2b8dc607
Anton Chernokulskyi <ton7.ok@icloud.com>
2022-04-01 13:35:40 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,75 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+enum SortBy {
+  default,
+  alphabet,
+  length,
+}
 
-export default App;
+export const App: React.FC = () => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [isReversed, setIsReversed] = useState(false);
+  const [sortBy, setSortBy] = useState(SortBy.default);
+
+  const start = () => setIsStarted(true);
+  const reverse = () => setIsReversed(!isReversed);
+  const setSortMethod = (by: SortBy) => setSortBy(by);
+  const reset = () => {
+    setIsReversed(false);
+    setSortBy(SortBy.default);
+  };
+
+  const setGoods = (initialGoods: string[]) => {
+    const preparedGoods = [...initialGoods];
+
+    preparedGoods.sort((a, b) => {
+      switch (sortBy) {
+        case SortBy.alphabet:
+          return a.localeCompare(b);
+        case SortBy.length:
+          return a.length - b.length;
+        case SortBy.default:
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      preparedGoods.reverse();
+    }
+
+    return preparedGoods;
+  };
+
+  const goods = setGoods(goodsFromServer);
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {!isStarted
+        ? <button type="button" onClick={start}>start</button>
+        : (
+          <>
+            <button type="button" onClick={reverse}>
+              reverse
+            </button>
+
+            <button type="button" onClick={() => setSortMethod(SortBy.alphabet)}>
+              sort by abc
+            </button>
+
+            <button type="button" onClick={() => setSortMethod(SortBy.length)}>
+              sort by len
+            </button>
+
+            <button type="button" onClick={reset}>
+              reset
+            </button>
+
+            <GoodsList goods={goods} />
+          </>
+        )}
+    </div>
+  );
+};
