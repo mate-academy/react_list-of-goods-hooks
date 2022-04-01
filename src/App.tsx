5c630ccd68b2f94823241e050b338a1d10bf118e
addKiwi <skrypak1997@gmail.com>
2022-04-01 21:08:35 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isVisible, setIsVisible] = useState(false);
 
-export default App;
+  const showList = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const sortReverse = () => {
+    setGoods([...goods].reverse());
+  };
+
+  const sort = (by: string) => {
+    const newGoods = [...goods];
+
+    newGoods.sort((goodA, goodB) => {
+      switch (by) {
+        case 'alph':
+          return goodA.localeCompare(goodB);
+
+        case 'len':
+          return goodA.length - goodB.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    return setGoods(newGoods);
+  };
+
+  const reset = () => {
+    setGoods(goodsFromServer);
+  };
+
+  return (
+    <div className="app">
+      <h1>List of Goods</h1>
+      { !isVisible
+        ? (
+          <button
+            type="button"
+            onClick={showList}
+          >
+            Start
+          </button>
+        )
+        : (
+          <>
+            <button
+              type="button"
+              onClick={sortReverse}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              onClick={() => sort('alph')}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              onClick={() => sort('len')}
+            >
+              Sort by Length
+            </button>
+            <button
+              type="button"
+              onClick={reset}
+            >
+              Reset
+            </button>
+            <ul>
+              {goods.map(good => (
+                <li
+                  key={good}
+                  className="list"
+                >
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+    </div>
+  );
+};
