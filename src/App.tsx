f39aec26f8eef3b6ae5eb46b4a70bc53b078ac79
Anna Serdechna <anserdechna@gmail.com>
2022-04-01 14:23:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-const goodsFromServer: string[] = [
+interface Good {
+  name: string;
+  id:string;
+}
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -12,13 +18,86 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(good => ({
+  name: good,
+  id: uuidv4(),
+}));
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [isListVisible, setIsListVisible] = useState(false);
+
+  const sortedByAlphbet = () => {
+    const sorted = [...goods].sort((a, b) => a.name.localeCompare(b.name));
+
+    setGoods(sorted);
+  };
+
+  const sortedByLength = () => {
+    const sorted = [...goods].sort((a, b) => a.name.length - b.name.length);
+
+    setGoods(sorted);
+  };
+
+  const showList = () => {
+    setIsListVisible(!isListVisible);
+  };
+
+  const reverse = () => {
+    setGoods([...goods].reverse());
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  return (
+    <div className="App">
+      <button
+        type="button"
+        onClick={showList}
+      >
+        Start
+      </button>
+
+      <button
+        type="button"
+        onClick={reverse}
+      >
+        Reverse
+      </button>
+
+      <button
+        type="button"
+        onClick={sortedByAlphbet}
+      >
+        Sort Alphabetically
+      </button>
+
+      <button
+        type="button"
+        onClick={sortedByLength}
+      >
+        Sort By Length
+      </button>
+
+      <button
+        type="button"
+        onClick={reset}
+      >
+        reset
+      </button>
+
+      {isListVisible
+          && (
+            <ul>
+              {goods.map(good => (
+                <li key={good.id}>{good.name}</li>
+              ))}
+            </ul>
+          )}
+    </div>
+  );
+};
 
 export default App;
