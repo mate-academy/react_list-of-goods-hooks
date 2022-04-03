9b8a4a0f52c889e7f73021e9d28ddf0673c69a6f
Vlad Brynzya <vladbrynzya@gmail.com>
2022-04-03 22:33:27 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,75 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export const App: React.FC = () => {
+  const [isGoodsVisible, setIsGoodsVisible] = useState(false);
+  const [reversedGoods, setIsReversedGoods] = useState(false);
+  const [sortBy, setSortBy] = useState('');
+  const copyGoods = [...goodsFromServer];
+
+  const showGoods = () => {
+    setIsGoodsVisible(!isGoodsVisible);
+  };
+
+  const reverse = () => {
+    setIsReversedGoods(!reversedGoods);
+  };
+
+  const sortByAlphabet = () => {
+    setSortBy('Alphabet');
+  };
+
+  const sortByLength = () => {
+    setSortBy('Length');
+  };
+
+  const reset = () => {
+    setIsGoodsVisible(!isGoodsVisible);
+    setIsReversedGoods(false);
+    setSortBy('');
+  };
+
+  copyGoods.sort((a, b) => {
+    switch (sortBy) {
+      case ('Alphabet'):
+        return a.localeCompare(b);
+      case ('Length'):
+        return a.length - b.length;
+      default:
+        return 0;
+    }
+  });
+
+  if (reversedGoods) {
+    copyGoods.reverse();
+  }
+
+  return (
+    <>
+      {isGoodsVisible
+        ? (
+          <>
+            <ul>
+              {copyGoods.map(good => (
+                <li key={good}>{good}</li>
+              ))}
+            </ul>
+            <button type="button" onClick={reverse}>Reverse</button>
+            <button type="button" onClick={sortByAlphabet}>Sort alphabetically</button>
+            <button type="button" onClick={sortByLength}>Sort by length</button>
+            <button type="button" onClick={reset}>Reset</button>
+          </>
+        )
+        : (
+          <button
+            type="button"
+            onClick={showGoods}
+          >
+            Start
+          </button>
+        )}
+    </>
+  );
+};
 
 export default App;
