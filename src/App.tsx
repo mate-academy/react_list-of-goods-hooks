84c42d293920a9f53f791da6ea840430a6fe67fb
Nazar Ostafiichuk <ostafii4uk@gmail.com>
2022-04-03 15:22:52 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 
-const goodsFromServer: string[] = [
+interface Good {
+  name: string,
+  id: string,
+}
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -12,13 +18,109 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(good => ({
+  name: good,
+  id: uuidv4(),
+}));
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export const App: React.FC = () => {
+  const [isReversed, setReversed] = useState(false);
+  const [sortBy, setSortBy] = useState('none');
+  const [isVisible, setVisible] = useState(false);
 
-export default App;
+  const visibilityApp = () => {
+    setVisible(true);
+  };
+
+  const getVisibilityGoods = () => {
+    const copyGoods = [...goodsFromServer];
+
+    const preperedCreater = copyGoods.sort((firstGood, secondGood) => {
+      switch (sortBy) {
+        case 'name':
+          return firstGood[sortBy].localeCompare(secondGood[sortBy]);
+        case 'length':
+          return firstGood.name.length - secondGood.name.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      copyGoods.reverse();
+    }
+
+    return preperedCreater;
+  };
+
+  const reverseGoods = () => {
+    setReversed(current => !current);
+  };
+
+  const sortByName = () => {
+    setSortBy('name');
+  };
+
+  const sortByLength = () => {
+    setSortBy('length');
+  };
+
+  const resetGoods = () => {
+    setSortBy('none');
+    setReversed(false);
+  };
+
+  return (!isVisible
+    ? (
+      <button
+        type="button"
+        onClick={visibilityApp}
+      >
+        Select
+      </button>
+    )
+    : (
+      <>
+        <h1>List of Goods</h1>
+        <ul>
+          {getVisibilityGoods()
+            .map(good => {
+              return (
+                <li key={good.id}>
+                  {good.name}
+                </li>
+              );
+            })}
+        </ul>
+
+        <button
+          type="button"
+          onClick={reverseGoods}
+        >
+          Reverse
+        </button>
+
+        <button
+          onClick={sortByName}
+          type="button"
+        >
+          Sort by ABC
+        </button>
+
+        <button
+          onClick={sortByLength}
+          type="button"
+        >
+          Sort by Length
+        </button>
+
+        <button
+          onClick={resetGoods}
+          type="button"
+        >
+          Reset
+        </button>
+      </>
+    )
+  );
+};
