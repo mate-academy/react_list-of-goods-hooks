f44cf304a4f8af239ca5a665254f62f5309a29c8
Dina Samoilova <s13.dina@gmail.com>
2022-04-12 11:42:09 +0300

    add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { GoodList } from './component/GoodsList';
-
-type Options = {
-  value: number,
-  label: string,
-};
-
-const goodsFromServer: string[] = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
-
-const options: Options[] = [
-  {
-    value: 1,
-    label: '1',
-  },
-  {
-    value: 2,
-    label: '2',
-  },
-  {
-    value: 3,
-    label: '3',
-  },
-  {
-    value: 4,
-    label: '4',
-  },
-  {
-    value: 5,
-    label: '5',
-  },
-  {
-    value: 6,
-    label: '6',
-  },
-  {
-    value: 7,
-    label: '7',
-  },
-  {
-    value: 8,
-    label: '8',
-  },
-  {
-    value: 9,
-    label: '9',
-  },
-  {
-    value: 10,
-    label: '10',
-  },
-];
+import { goodsFromServer, options } from './constants';
 
 const App: React.FC = () => {
   const emtyArray: string[] = [];
---
+++ src/types/type.ts
@@ -0,0 +1,12 @@
+export type Options = {
+  value: number,
+  label: string,
+};
+
+export type Props = {
+  goods: string[] | [],
+  isReversed: boolean,
+  sorted: boolean,
+  lengthSort: boolean,
+  choosedLength: number,
+};
