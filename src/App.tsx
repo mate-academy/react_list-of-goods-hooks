6a89cb610583945feda5a3e4e9b1360d622873e8
KristinaTar <kristina.tarashchuk@gmail.com>
2022-04-12 14:55:34 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +13,79 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC<{}> = () => {
+  const [jsx, setJsx] = useState<JSX.Element[]>([]);
+
+  const showGoods = () => {
+    setJsx(goodsFromServer.map(good => (
+      <li>
+        {good}
+      </li>
+    )));
+  };
+
+  const reverseGoods = () => {
+    const jsxCopy = [...jsx];
+
+    setJsx(jsxCopy.reverse());
+  };
+
+  const sortAlphabetically = () => {
+    const goodsCopy = [...goodsFromServer];
+
+    goodsCopy.sort(
+      (a, b) => a.localeCompare(b),
+    );
+
+    setJsx(goodsCopy.map(good => (
+      <li>
+        {good}
+      </li>
+    )));
+  };
+
+  const reset = () => {
+    showGoods();
+  };
+
+  const sortByNameLength = () => {
+    const goodsCopy = [...goodsFromServer];
+
+    goodsCopy.sort(
+      (a, b) => a.length - b.length,
+    );
+
+    setJsx(goodsCopy.map(good => (
+      <li>
+        {good}
+      </li>
+    )));
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {
+        jsx.length === 0
+          ? <button type="button" onClick={showGoods}>Start</button>
+          : (
+            <div>
+              <button type="button" onClick={reverseGoods}>
+                Reverse
+              </button>
+              <button type="button" onClick={sortAlphabetically}>
+                Sort alphabetically
+              </button>
+              <button type="button" onClick={reset}>Reset</button>
+              <button type="button" onClick={sortByNameLength}>
+                Sort by length
+              </button>
+              {jsx}
+            </div>
+          )
+      }
+    </div>
+  );
+};
 
 export default App;
