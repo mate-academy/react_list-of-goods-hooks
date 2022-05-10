12074f27aebccaf7a27e7115ad20ca8eecbbd021
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-05-10 17:52:50 +0300

    List with hooks using was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,113 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isVisible, setIsVisible] = useState(false);
+  const [limit, setLimit] = useState(1);
+  const visibleGoods = goods.filter((good) => good.length >= limit);
+
+  const visibleList = () => {
+    setIsVisible((current) => !current);
+  };
+
+  const reverseList = () => {
+    setGoods((current) => [...current].reverse());
+  };
+
+  const sortByAlphabet = () => {
+    setGoods((current) => [...current].sort((g1, g2) => g1.localeCompare(g2)));
+  };
+
+  const sortByLength = () => {
+    setGoods((current) => [...current].sort((g1, g2) => g1.length - g2.length));
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  const changeLimit = (event: React.SyntheticEvent<EventTarget>) => {
+    setLimit(+(event.target as HTMLSelectElement).value);
+  };
+
+  const selectReset = () => {
+    setLimit(1);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="App__title">Goods list</h1>
+      {!isVisible && (
+        <button
+          type="button"
+          onClick={visibleList}
+          className="App__button Button__start"
+        >
+          Start
+        </button>
+      )}
+
+      {isVisible && (
+        <>
+          <GoodsList goods={visibleGoods} />
+          <div className="App__buttons">
+            <button
+              type="button"
+              onClick={reverseList}
+              className="App__button"
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={sortByAlphabet}
+              className="App__button"
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              onClick={sortByLength}
+              className="App__button"
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={reset}
+              className="App__button Button__reset"
+            >
+              Reset
+            </button>
+          </div>
+
+          <form className="App__form">
+            <select
+              value={limit}
+              onChange={changeLimit}
+              className="App__select"
+            >
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+                <option value={num} key={num}>{num}</option>
+              ))}
+            </select>
+
+            <button
+              type="button"
+              onClick={selectReset}
+              className="App__button Button__reset"
+            >
+              Reset select
+            </button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/GoodsList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './GoodsList.css';
+
+type Props = {
+  goods: string[];
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <div className="GoodsList__wrapper">
+    <ul className="GoodsList">
+      {goods.map((good) => (
+        <li key={good} className="GoodsList__link">
+          {good}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
