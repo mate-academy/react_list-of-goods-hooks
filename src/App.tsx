17067928dd1360c3af1e7a7fb59ab3fdee0eb390
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-05-11 20:45:37 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { List } from './components/List';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,28 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="app">
+      <h1 className="app__header">Goods</h1>
+
+      {isVisible && <List goodsList={goodsFromServer} />}
+
+      {
+        !isVisible && (
+          <button
+            type="button"
+            onClick={() => setIsVisible(true)}
+            className="show-button"
+          >
+            Start
+          </button>
+        )
+      }
+    </div>
+  );
+};
 
 export default App;
