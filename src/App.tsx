e5722a5a78bdadd0fc923d109ec2856b05e2f317
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-12 22:14:36 +0300

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ProductsList } from './ProductsList';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,77 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, setVisibeled] = useState(false);
+  const [isReversed, setReversed] = useState(false);
+  const [sortedBy, setSortedBy] = useState('');
+
+  const prepareProducts = () => {
+    const copyProducts = [...goodsFromServer];
+
+    copyProducts.sort((productA, productB) => {
+      switch (sortedBy) {
+        case 'alphabet':
+          return productA.localeCompare(productB);
+        case 'length':
+          return productA.length - productB.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      copyProducts.reverse();
+    }
+
+    return copyProducts;
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {!isVisible && (
+        <button
+          type="button"
+          onClick={() => setVisibeled(true)}
+        >
+          Start
+        </button>
+      )}
+      {isVisible && (
+        <>
+          <ProductsList products={prepareProducts()} />
+          <button
+            type="button"
+            onClick={() => setReversed(!isReversed)}
+          >
+            Revers
+          </button>
+          <button
+            type="button"
+            onClick={() => setSortedBy('alphabet')}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            onClick={() => setSortedBy('length')}
+          >
+            Sort by length
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setReversed(false);
+              setSortedBy('');
+            }}
+          >
+            Reset
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
