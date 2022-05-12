d476abd0bb9a39b738ce9472bcaba93955f43a96
Kirill Polozenko <kir.polozenko@icloud.com>
2022-05-12 18:36:58 +0300

    add components

---
+++ src/Components/GoodList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './GoodList.css';
+
+type Props = {
+  goods: string[],
+};
+
+export const GoodList:React.FC<Props> = ({ goods }) => {
+  return (
+    <ul className="good-list">
+      {goods.map((good) => (
+        <li className="good-list__item" key={good}>
+          {good}
+        </li>
+      ))}
+    </ul>
+  );
+};
