236ce622f3242d0b54764e21210a394021ff6b7b
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-05-14 10:22:30 +0300

    separately list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { GoodsList } from './GoodsList/GoodsList';
 import './App.css';
 
+const goodsLengths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -15,11 +16,110 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    <GoodsList goodsFromServer={goodsFromServer} />
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [sortGoods, setSortGoodsBy] = useState('');
+  const [goodsLength, setGoodsLength] = useState(1);
+  const [isReversed, setIsReversed] = useState(false);
+
+  const sortByAlphabet = () => {
+    setSortGoodsBy('Alphabet');
+  };
+
+  const sortByLength = () => {
+    setSortGoodsBy('Length');
+  };
+
+  const reset = () => {
+    setGoodsLength(1);
+    setIsReversed(false);
+    setSortGoodsBy('');
+  };
+
+  const reverse = () => {
+    setIsReversed(!isReversed);
+  };
+
+  const goodsList = useMemo(() => {
+    return [...goodsFromServer]
+      .filter(good => good.length >= goodsLength)
+      .sort((first, last) => {
+        switch (sortGoods) {
+          case 'Alphabet':
+            return first.localeCompare(last);
+
+          case 'Length':
+            return first.length - last.length;
+
+          default:
+            return 0;
+        }
+      });
+  }, [sortGoods, goodsLength]);
+
+  if (isReversed) {
+    goodsList.reverse();
+  }
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {!isVisible && (
+        <button
+          type="button"
+          className="button"
+          onClick={() => setIsVisible(true)}
+        >
+          Show goods
+        </button>
+      )}
+      {isVisible && (
+        <>
+          <div className="select">
+            Choose length for sorting:
+            <select
+              name="good"
+              id="good"
+              onChange={event => setGoodsLength(+event.target.value)}
+            >
+              {goodsLengths.map(length => (
+                <option value={length} key={length}>{length}</option>
+              ))}
+            </select>
+          </div>
+          <button
+            type="button"
+            className="button"
+            onClick={reverse}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={sortByAlphabet}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={sortByLength}
+          >
+            Sort by length
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={reset}
+          >
+            Reset
+          </button>
+          <GoodsList goods={goodsFromServer} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/GoodsList/GoodsList.tsx
+++ src/GoodsList/GoodsList.tsx
@@ -1,121 +1,22 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 import './GoodsList.scss';
 
-const goodsLengths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 type Props = {
-  goodsFromServer: string[]
+  goods: string[]
 };
 
-export const GoodsList: React.FC<Props> = ({ goodsFromServer }) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const [sortGoods, setSortGoodsBy] = useState('');
-  const [goodsLength, setGoodsLength] = useState(1);
-  const [isReversed, setIsReversed] = useState(false);
-
-  const sortByAlphabet = () => {
-    setSortGoodsBy('Alphabet');
-  };
-
-  const sortByLength = () => {
-    setSortGoodsBy('Length');
-  };
-
-  const reset = () => {
-    setGoodsLength(1);
-    setIsReversed(false);
-    setSortGoodsBy('');
-  };
-
-  const reverse = () => {
-    setIsReversed(!isReversed);
-  };
-
-  const goodsList = useMemo(() => {
-    return [...goodsFromServer]
-      .filter(good => good.length >= goodsLength)
-      .sort((first, last) => {
-        switch (sortGoods) {
-          case 'Alphabet':
-            return first.localeCompare(last);
-
-          case 'Length':
-            return first.length - last.length;
-
-          default:
-            return 0;
-        }
-      });
-  }, [sortGoods, goodsLength]);
-
-  if (isReversed) {
-    goodsList.reverse();
-  }
-
+export const GoodsList: React.FC<Props> = ({ goods }) => {
   return (
-    <div>
-      {!isVisible && (
-        <button
-          type="button"
-          className="button"
-          onClick={() => setIsVisible(true)}
-        >
-          Show goods
-        </button>
-      )}
-      {isVisible && (
-        <>
-          <div className="select">
-            Choose length for sorting:
-            <select
-              name="good"
-              id="good"
-              onChange={event => setGoodsLength(+event.target.value)}
-            >
-              {goodsLengths.map(length => (
-                <option value={length} key={length}>{length}</option>
-              ))}
-            </select>
-          </div>
-          <button
-            type="button"
-            className="button"
-            onClick={reverse}
-          >
-            Reverse
-          </button>
-          <button
-            type="button"
-            className="button"
-            onClick={sortByAlphabet}
-          >
-            Sort alphabetically
-          </button>
-          <button
-            type="button"
-            className="button"
-            onClick={sortByLength}
-          >
-            Sort by length
-          </button>
-          <button
-            type="button"
-            className="button"
-            onClick={reset}
-          >
-            Reset
-          </button>
-          <ul className="list">
-            <h1>List of goods:</h1>
-            {goodsList.map(good => (
-              <li key={good} className="list__item">
-                <span>{good}</span>
-                <span>{`length: ${good.length}`}</span>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-    </div>
+    <>
+      <ul className="list">
+        <h1>List of goods:</h1>
+        {goods.map(good => (
+          <li key={good} className="list__item">
+            <span>{good}</span>
+            <span>{`length: ${good.length}`}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
