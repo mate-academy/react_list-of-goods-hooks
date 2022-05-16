80f4b7b6186dbf43f8d5aedf9c40d2bccfdd454e
milanamarkovska <milamo228322@gmail.com>
2022-05-16 14:14:02 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+import './App.scss';
+import { GoodsList } from './GoodsList/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,91 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const lengthByDefault = 1;
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isVisible, setVisible] = useState(false);
+  const [limit] = useState(lengthByDefault);
+
+  const filterGoods = (array: string[], value: number) => {
+    return array.filter((elem) => elem.length >= value);
+  };
+
+  const visibleGoods = useMemo(() => filterGoods(goods, limit),
+    [goods, limit]);
+
+  const visibleList = () => {
+    setVisible((current) => !current);
+  };
+
+  const reverseList = () => {
+    setGoods((current) => [...current].reverse());
+  };
+
+  const sortByAlphabet = () => {
+    setGoods((current) => [...current].sort((g1, g2) => g1.localeCompare(g2)));
+  };
+
+  const sortByLength = () => {
+    setGoods((current) => [...current].sort((g1, g2) => g1.length - g2.length));
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  return (
+    <div className="goods-list">
+      {!isVisible && (
+        <button
+          type="button"
+          onClick={visibleList}
+          className="goods-list__btn"
+        >
+          Start
+        </button>
+      )}
+
+      {isVisible && (
+        <>
+          {visibleGoods.length > 0 && <GoodsList goods={visibleGoods} />}
+          <div className="goods-list__btns">
+            <button
+              type="button"
+              onClick={reverseList}
+              className="goods-list__btn"
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={sortByAlphabet}
+              className="goods-list__btn"
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              onClick={sortByLength}
+              className="goods-list__btn"
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={reset}
+              className="goods-list__btn"
+            >
+              Reset
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
