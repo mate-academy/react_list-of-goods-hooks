debca6654fcddd451721376902222c37682eb778
unknown <avicostov2@gmail.com>
2022-05-23 12:14:39 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import GoodsList from './components/GoodsList';
+import { SortType } from './types/SortType';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,83 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isVisible, makeVisible] = useState(false);
+  const [isReversed, makeReverse] = useState(false);
+  const [SortBy, selectSort] = useState(SortType.Nothing);
+  const [lengthMin, changeMinimum] = useState(1);
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {isVisible && (
+        <>
+          <GoodsList
+            goods={goodsFromServer}
+            isReverse={isReversed}
+            SortBy={SortBy}
+            lengthMin={lengthMin}
+          />
+          <button
+            type="button"
+            onClick={() => makeReverse(!isReversed)}
+          >
+            Reverse
+          </button>
+          <br />
+          <button
+            type="button"
+            onClick={() => selectSort(SortType.Alfabet)}
+          >
+            Sort by alfabet
+          </button>
+          <button
+            type="button"
+            onClick={() => selectSort(SortType.Length)}
+          >
+            Sort by length
+          </button>
+          <br />
+          <button
+            type="button"
+            onClick={() => {
+              selectSort(SortType.Nothing);
+              makeReverse(false);
+              changeMinimum(1);
+            }}
+          >
+            Reset
+          </button>
+          <br />
+          <select
+            name="numbersSelect"
+            id="1"
+            onChange={({ currentTarget }) => (
+              changeMinimum(Number(currentTarget.value))
+            )}
+          >
+            {Array(10).fill(null).map((_, index) => (
+              <option
+                value={index + 1}
+              >
+                {index + 1}
+              </option>
+            ))}
+          </select>
+        </>
+      )}
+      {!isVisible && (
+        <button
+          type="button"
+          onClick={
+            () => makeVisible(true)
+          }
+        >
+          Start
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default App;
