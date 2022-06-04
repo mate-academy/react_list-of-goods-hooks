840a5754ba5287f9609c7113a243c209bb46d824
Andriy Fesych <andriyhimself@gmail.com>
2022-06-04 20:17:03 +0300

    Solution_0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { GoodsList } from './components/GoodsList';
+import { ButtonGenerator } from './components/ButtonGenerator';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,75 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+const App: React.FC<Props> = () => {
+  const [goodsList, setGoodsList] = useState([...goodsFromServer]);
+  const [visibility, setVisibility] = useState(false);
+  const [lengthLimit, setLengthLimit] = useState(1);
+
+  const visibilitySwitch = () => {
+    setVisibility(prevVis => !prevVis);
+  };
+
+  const reverseList = () => {
+    setGoodsList(prevList => [...prevList].reverse());
+  };
+
+  const sortAlphabetically = () => {
+    setGoodsList(prevList => [...prevList].sort(
+      (good1, good2) => good1.localeCompare(good2),
+    ));
+  };
+
+  const resetList = () => {
+    setGoodsList([...goodsFromServer]);
+    setLengthLimit(1);
+  };
+
+  const sortbyLength = () => {
+    setGoodsList(prevList => [...prevList].sort(
+      (good1, good2) => good1.length - good2.length,
+    ));
+  };
+
+  const changeLimit = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setLengthLimit(+event.target.value);
+    setGoodsList([...goodsFromServer]
+      .filter(good => good.length >= +event.target.value));
+  };
+
+  return (
+    <div className="App has-text-centered">
+      <br />
+      <h1 className="title is-1">Goods list sorting</h1>
+      {!visibility
+        && <ButtonGenerator name="Start" method={visibilitySwitch} />}
+      {visibility
+      && (
+        <>
+          <ButtonGenerator name="Reverse" method={reverseList} />
+          <ButtonGenerator
+            name="Sort alphabetically"
+            method={sortAlphabetically}
+          />
+          <ButtonGenerator name="Reset" method={resetList} />
+          <ButtonGenerator name="Sort by length" method={sortbyLength} />
+          <div className="select is-rounded is-info is-light is-outlined">
+            <select
+              value={lengthLimit}
+              onChange={changeLimit}
+            >
+              {Array(10).fill('option').map((item, index) => (
+                <option key={`${item}${index + 1}`} value={index + 1}>{index + 1}</option>
+              ))}
+            </select>
+          </div>
+          <GoodsList goods={goodsList} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
