2d6f2c6ae84c332507f033e6f0fa28d1e7297807
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-06-07 00:45:02 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,67 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [listIsVisible, setListIsVisible] = useState(false);
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isReversed, setIsReversed] = useState(false);
+
+  const reverseGoods = () => {
+    setIsReversed(!isReversed);
+    setGoods(current => [...current].reverse());
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+      {console.log(isReversed)}
+      {listIsVisible && (
+        <>
+          <GoodsList goods={goods} />
+
+          <button
+            type="button"
+            onClick={() => reverseGoods()}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setGoods(
+              [...goodsFromServer].sort((a, b) => a.localeCompare(b)),
+            )}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setGoods([...goodsFromServer])}
+          >
+            Reset
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setGoods(
+              [...goodsFromServer].sort((a, b) => a.length - b.length),
+            )}
+          >
+            Sort by length
+          </button>
+        </>
+      )}
+      {!listIsVisible && (
+        <button
+          type="button"
+          onClick={() => setListIsVisible(true)}
+        >
+          Start
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default App;
