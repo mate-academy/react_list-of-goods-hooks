02d4f48ea5e28fc249f222d17bd5e099eab06ae7
Volodymyr <lesksv2000@gmail.com>
2022-06-06 22:24:03 +0300

    first hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
+import 'bulma/css/bulma.min.css';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,93 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const reverse = () => {
+    setGoods(prevState => [...prevState].reverse());
+  };
+
+  const sortAlphabetically = () => {
+    setGoods(prevState => [...prevState]
+      .sort((a, b) => a.localeCompare(b)));
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  const sortByLength = () => {
+    setGoods(prevState => [...prevState]
+      .sort((a, b) => a.length - b.length));
+  };
+
+  const showStart = () => {
+    setIsVisible(prevState => !prevState);
+  };
+
+  return (
+    <div className="app">
+      {
+        !isVisible
+          ? (
+            <div className="button-block">
+              <button
+                type="button"
+                onClick={showStart}
+                className="button is-success is-focused box"
+              >
+                Start
+              </button>
+            </div>
+          )
+          : (
+
+            <div className="block app-container">
+              <div className="app-list has-text-centered">
+                <GoodsList goods={goods} />
+
+                <div className="button-container">
+                  <button
+                    type="button"
+                    onClick={reverse}
+                    className="mgr-small button is-success is-focused mr-5"
+                  >
+                    Reverse
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={sortAlphabetically}
+                    className="button is-success is-focused mgr-small mr-5"
+                  >
+                    Sort alphabetically
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={reset}
+                    className="button is-success is-focused mr-5"
+                  >
+                    Reset
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={sortByLength}
+                    className="button is-success is-focused mr-5"
+                  >
+                    Sort by length
+                  </button>
+
+                </div>
+              </div>
+            </div>
+          )
+      }
+    </div>
+  );
+};
 
 export default App;
