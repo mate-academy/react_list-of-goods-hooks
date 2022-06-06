2fb5848842c524b32cf977e3d9ce2872ab8684ab
foxifio <foxifio00@gmail.com>
2022-06-06 18:54:44 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { GoodsList } from './components';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,85 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [isGoodsVisible, setIsGoodsVisible] = useState(false);
+
+  const showGoods = () => {
+    setIsGoodsVisible(current => !current);
+  };
+
+  const reverseGoods = () => {
+    setGoods(current => [...current].reverse());
+  };
+
+  const sortAlphabetically = () => {
+    setGoods(current => [...current]
+      .sort((firstGood, secondGood) => firstGood.localeCompare(secondGood)));
+  };
+
+  const sortByLength = () => {
+    setGoods(current => [...current]
+      .sort((firstGood, secondGood) => firstGood.length - secondGood.length));
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  return (
+    <div className="container">
+      <button
+        type="button"
+        className={isGoodsVisible
+          ? 'btn-danger'
+          : 'btn-primary'}
+        onClick={showGoods}
+      >
+        {isGoodsVisible
+          ? 'Hide goods'
+          : 'Show goods'}
+      </button>
+      {isGoodsVisible
+      && (
+        <GoodsList goods={goods} />
+      )}
+      {isGoodsVisible
+      && (
+        <>
+          <button
+            type="button"
+            className="btn-primary"
+            onClick={reverseGoods}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            className="btn-primary"
+            onClick={sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            className="btn-primary"
+            onClick={sortByLength}
+          >
+            Sort by length
+          </button>
+          <button
+            type="button"
+            className="btn-primary"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </>
+
+      )}
+    </div>
+  );
+};
 
 export default App;
