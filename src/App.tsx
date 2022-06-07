e0c76fce3c9ebc61b0a5c205be6dc5c8f9b7db7c
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-06-07 20:37:22 +0300

    Task Done v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +14,106 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [visibleList, setVisibleList] = useState(false);
 
-export default App;
+  const isVisibleList = () => {
+    setVisibleList(!visibleList);
+  };
+
+  const makeReverse = () => {
+    setGoods([...goods].reverse());
+  };
+
+  const makeSortAlphabet = () => {
+    setGoods([...goods].sort((a, b) => a.localeCompare(b)));
+  };
+
+  const makeSortLength = () => {
+    setGoods([...goods].sort((a, b) => a.length - b.length));
+  };
+
+  const makeReset = () => {
+    setGoods(goodsFromServer);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">Goods</h1>
+
+      {!visibleList && (
+        <button
+          type="button"
+          className="
+            button
+            is-success
+            button--start
+          "
+          onClick={isVisibleList}
+        >
+          Start
+        </button>
+      )}
+
+      {visibleList && (
+        <ul className="list">
+          <button
+            type="button"
+            onClick={isVisibleList}
+            className="
+              delete
+              is-large
+              button--close"
+          >
+            Close List
+          </button>
+
+          {goods.map((good) => (
+            <li className="list-item" key={good}>
+              {good}
+            </li>
+          ))}
+
+          <div className="buttonWrapper">
+            <button
+              type="button"
+              onClick={makeReverse}
+              className="button is-info"
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={makeSortAlphabet}
+              className="button is-info"
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              onClick={makeSortLength}
+              className="button is-info"
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={makeReset}
+              className="
+                button
+                is-danger
+                is-light
+              "
+            >
+              Reset
+            </button>
+          </div>
+        </ul>
+      )}
+    </div>
+  );
+};
