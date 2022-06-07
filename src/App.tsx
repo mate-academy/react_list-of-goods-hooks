4a6a3a4c0f6c6ea3e265a3e43a0f7fedd1716db3
Vicky Miziukanova <enrikadej@gmail.com>
2022-06-07 15:36:12 +0300

    task without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,80 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isGoodsVisible, setIsGoodsVisible] = useState(false);
+
+  const showList = () => {
+    setIsGoodsVisible(!isGoodsVisible);
+  };
+
+  const reverse = () => {
+    setGoods([...goods].reverse());
+  };
+
+  const sortByAbc = () => {
+    setGoods([...goods].sort());
+  };
+
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  const sortByLength = () => {
+    setGoods([...goods].sort((pr1, pr2) => pr1.length - pr2.length));
+  };
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+
+      <button
+        type="button"
+        onClick={showList}
+      >
+        Start
+      </button>
+
+      <button
+        type="button"
+        onClick={reverse}
+      >
+        Reverse
+      </button>
+
+      <button
+        type="button"
+        onClick={sortByAbc}
+      >
+        Sort alphavetically
+      </button>
+
+      <button
+        type="button"
+        onClick={sortByLength}
+      >
+        Sort by length
+      </button>
+
+      <button
+        type="button"
+        onClick={reset}
+      >
+        Reset
+      </button>
+
+      {isGoodsVisible && (
+        <ul>
+          {goods.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default App;
