aebcef62b66550a3d6f70df08c7955b4d0558473
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2022-06-07 08:51:21 +0300

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,100 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showGoods = () => {
+    setIsVisible(current => !current);
+  };
+
+  const hideGoods = () => {
+    setIsVisible(current => !current);
+    setGoods(goodsFromServer);
+  };
+
+  const reverseGoods = () => {
+    setGoods(current => [...current].reverse());
+  };
+
+  const sortByAlphabet = () => {
+    setGoods(current => [...current]
+      .sort((a, b) => a.localeCompare(b)));
+  };
+
+  const sortByLength = () => {
+    setGoods(current => [...current]
+      .sort((a, b) => a.length - b.length));
+  };
+
+  const reset = () => {
+    setGoods(goodsFromServer);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="app__title">Goods</h1>
+      {!isVisible
+      && (
+        <button
+          type="button"
+          className="btn"
+          onClick={showGoods}
+        >
+          Start
+        </button>
+      )}
+
+      {isVisible
+      && (
+        <section className="goods">
+          <GoodsList goods={goods} />
+          <div className="buttons">
+            <button
+              type="button"
+              className="btn-item"
+              onClick={reverseGoods}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              className="btn-item"
+              onClick={reset}
+            >
+              Reset
+            </button>
+            <button
+              type="button"
+              className="btn-item"
+              onClick={sortByAlphabet}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              className="btn-item"
+              onClick={sortByLength}
+            >
+              Sort by length
+            </button>
+          </div>
+        </section>
+      )}
+
+      {isVisible
+      && (
+        <button
+          type="button"
+          className="btn btn--hide"
+          onClick={hideGoods}
+        >
+          End
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default App;
