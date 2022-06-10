01670f0742cc634a1a1071f04415f42fab104535
Swazi Kunene <kuneneswazi18@gmail.com>
2022-06-10 17:27:49 +0200

    completed optional task

--- src/App.tsx
+++ src/App.tsx
@@ -21,30 +21,45 @@ enum SortBy {
 }
 
 const App: React.FC = () => {
+  const [goods] = useState([...goodsFromServer]);
   const [isStarted, setStart] = useState(false);
   const [isReversed, reverse] = useState(false);
   const [sortBy, setSortBy] = useState(SortBy.None);
+  const [length, setLength] = useState(1);
 
-  let visibleGoods = [...goodsFromServer];
+  const prepareList = (
+    goodsArray: string[],
+    selectedLength: number,
+    sortType: string,
+  ) => {
+    const preparedList = [...goodsArray].filter(good => (
+      good.length >= selectedLength));
 
-  switch (sortBy) {
-    case SortBy.Name:
-      visibleGoods = visibleGoods.sort((good1, good2) => (
-        good1.localeCompare(good2)));
-      break;
+    switch (sortType) {
+      case SortBy.Name:
+        preparedList.sort((good1, good2) => (
+          (good1.toLowerCase()).localeCompare(good2.toLowerCase())
+        ));
+        break;
 
-    case SortBy.Length:
-      visibleGoods = visibleGoods.sort((good1, good2) => (
-        good1.length - good2.length));
-      break;
+      case SortBy.Length:
+        preparedList.sort((good1, good2) => (
+          good1.length - good2.length
+        ));
+        break;
 
-    default:
-      visibleGoods = [...goodsFromServer];
-  }
+      default:
+        break;
+    }
 
-  if (isReversed) {
-    visibleGoods.reverse();
-  }
+    if (isReversed) {
+      preparedList.reverse();
+    }
+
+    return preparedList;
+  };
+
+  const visibleGoods = prepareList(goods, length, sortBy);
 
   return (
     <div className="App container.is-widescreen has-text-centered">
@@ -59,45 +74,63 @@ const App: React.FC = () => {
         </button>
       )}
 
-      <div className="is-flex is-justify-content-center mt-5">
-        <button
-          type="button"
-          className="button is-info mx-4"
-          onClick={() => reverse(!isReversed)}
-        >
-          Reverse
-        </button>
-
-        <button
-          type="button"
-          className="button is-info mx-4"
-          onClick={() => setSortBy(SortBy.Name)}
-        >
-          Sort Alphabetically
-        </button>
-
-        <button
-          type="button"
-          className="button is-info mx-4"
-          onClick={() => setSortBy(SortBy.Length)}
-        >
-          Sort by length
-        </button>
-
-        <button
-          type="button"
-          className="button is-info mx-4"
-          onClick={() => {
-            reverse(false);
-            setSortBy(SortBy.None);
-          }}
-        >
-          Reset
-        </button>
-      </div>
-
       {isStarted && (
         <div className="mt-6">
+          <div className="is-flex is-justify-content-center mt-5">
+            <button
+              type="button"
+              className="button is-info mx-4"
+              onClick={() => reverse(!isReversed)}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              className="button is-info mx-4"
+              onClick={() => setSortBy(SortBy.Name)}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              className="button is-info mx-4"
+              onClick={() => setSortBy(SortBy.Length)}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              className="button is-info mx-4"
+              onClick={() => {
+                reverse(false);
+                setSortBy(SortBy.None);
+                setLength(1);
+              }}
+            >
+              Reset
+            </button>
+
+            <select
+              name="length"
+              value={length}
+              onChange={(event) => setLength(+event.target.value)}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
           <h2 className="title is-2">Goods List:</h2>
 
           <ul>
