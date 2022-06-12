f649238efa3f7a53719a14289860a339d39c79e2
Lebogang Mogane <mogane420@gmail.com>
2022-06-12 23:10:54 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,83 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isListAvailable, showList] = useState(false);
+  const [isReversed, reverseList] = useState(false);
+  const [sortBy, sortList] = useState('');
+
+  const goodsCopy = [...goodsFromServer];
+
+  switch (sortBy) {
+    case 'alphabetically':
+      goodsCopy.sort();
+      break;
+    case 'byLength':
+      goodsCopy.sort((a, b) => a.length - b.length);
+      break;
+    default:
+      break;
+  }
+
+  if (isReversed) {
+    goodsCopy.reverse();
+  }
+
+  return (
+    <div className="App">
+      <h1>Goods</h1>
+
+      {goodsFromServer.length}
+
+      <div>
+        <button
+          type="button"
+          onClick={() => reverseList(true)}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          onClick={() => sortList('alphabetically')}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          onClick={() => sortList('byLength')}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            sortList('');
+            reverseList(false);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+
+      {isListAvailable
+        ? (
+          <ul>
+            {goodsCopy.map(goodItem => <li key={goodItem}>{goodItem}</li>)}
+          </ul>
+        )
+        : (
+          <button
+            type="button"
+            onClick={() => showList(true)}
+          >
+            Start
+          </button>
+        )}
+    </div>
+  );
+};
 
 export default App;
