b50432082a99f384bdcebfb8c93db97166e9a5d7
Elina Kovalenko <kovalenkoelin@gmail.com>
2022-06-28 09:10:19 +0300

    seperate function for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,34 @@ const App: React.FC = () => {
   const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
   const [minLength, changeMinLength] = useState(1);
 
+  const getReversedGoods = () => {
+    setVisibleGoods([...visibleGoods].reverse());
+  };
+
+  const getFiteredGoods = (length: number) => {
+    changeMinLength(length);
+    setVisibleGoods(goodsFromServer.filter(good => (
+      good.length >= length
+    )));
+  };
+
+  const getSortedGoods = (method: string) => {
+    if (method === 'abc') {
+      setVisibleGoods([...visibleGoods].sort((a, b) => (
+        a.localeCompare(b)
+      )));
+    } else {
+      setVisibleGoods([...visibleGoods].sort((a, b) => (
+        a.length - b.length
+      )));
+    }
+  };
+
+  const resetGoods = () => {
+    setVisibleGoods(goodsFromServer);
+    changeMinLength(1);
+  };
+
   return (
     <div
       className="App box container"
@@ -28,98 +56,90 @@ const App: React.FC = () => {
       >
         Goods
       </h1>
-      {!goodsIsVisible && (
-        <button
-          className="button is-success is-outlined"
-          type="button"
-          onClick={() => showGoods(true)}
-        >
-          Start
-        </button>
-      )}
-      {goodsIsVisible && (
-        <>
-          <ul>
-            {visibleGoods.map(good => {
-              return (
-                <li
-                  key={good}
-                  className="has-text-centered"
-                >
-                  {good}
-                </li>
-              );
-            })}
-          </ul>
+      {!goodsIsVisible
+        ? (
+          <button
+            className="button is-success is-outlined"
+            type="button"
+            onClick={() => showGoods(true)}
+          >
+            Start
+          </button>
+        )
+        : (
+          <>
+            <ul>
+              {visibleGoods.map(good => {
+                return (
+                  <li
+                    key={good}
+                    className="has-text-centered"
+                  >
+                    {good}
+                  </li>
+                );
+              })}
+            </ul>
 
-          <div className="is-flex">
-            <button
-              className="button"
-              type="button"
-              onClick={() => setVisibleGoods([...visibleGoods].reverse())}
-            >
-              Reverse
-            </button>
+            <div className="is-flex">
+              <button
+                className="button"
+                type="button"
+                onClick={() => getReversedGoods()}
+              >
+                Reverse
+              </button>
 
-            <button
-              className="button"
-              type="button"
-              onClick={() => setVisibleGoods([...visibleGoods].sort((a, b) => (
-                a.localeCompare(b)
-              )))}
-            >
-              Sort alphabetically
-            </button>
+              <button
+                className="button"
+                type="button"
+                onClick={() => getSortedGoods('abc')}
+              >
+                Sort alphabetically
+              </button>
 
-            <button
-              className="button"
-              type="button"
-              onClick={() => setVisibleGoods([...visibleGoods].sort((a, b) => (
-                a.length - b.length
-              )))}
-            >
-              Sort by length
-            </button>
+              <button
+                className="button"
+                type="button"
+                onClick={() => getSortedGoods('length')}
+              >
+                Sort by length
+              </button>
 
-            <select
-              className="button"
-              name=""
-              id=""
-              value={minLength}
-              onChange={(e) => {
-                setVisibleGoods(goodsFromServer.filter(good => (
-                  good.length >= +e.target.value
-                )));
-                changeMinLength(+e.target.value);
-              }}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+              <select
+                className="button"
+                name=""
+                id=""
+                value={minLength}
+                onChange={(e) => {
+                  getFiteredGoods(+e.target.value);
+                }}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
 
-          {visibleGoods !== goodsFromServer && (
-            <button
-              className="button is-danger is-outlined is-fullwidth"
-              type="button"
-              onClick={() => {
-                setVisibleGoods(goodsFromServer);
-                changeMinLength(1);
-              }}
-            >
-              Reset
-            </button>
-          )}
-        </>
-      )}
+            {visibleGoods !== goodsFromServer && (
+              <button
+                className="button is-danger is-outlined is-fullwidth"
+                type="button"
+                onClick={() => resetGoods()}
+              >
+                Reset
+              </button>
+            )}
+          </>
+        )
+      }
     </div>
   );
 };
