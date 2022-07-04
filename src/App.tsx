e26efe2847359932dd45f42cb788f6eb110b4e47
Dima <d.maik2178@gmail.com>
2022-07-05 01:29:34 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,28 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [value, setValue] = useState(false);
+
+  const start = () => {
+    setValue(current => !current);
+  };
+
+  return (
+    <div className="App">
+      {!value && (
+        <button
+          type="button"
+          onClick={start}
+        >
+          Start
+        </button>
+      )}
+      {value && (
+        <GoodsList goods={goodsFromServer} />
+      )}
+    </div>
+  );
+};
 
 export default App;
