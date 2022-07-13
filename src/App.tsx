5d86791d1a9c0f50e18973660e8c11318762bbc3
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-07-13 21:08:16 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -17,74 +17,21 @@ const goodsFromServer: string[] = [
 
 const App: React.FC = () => {
   const [isShown, setIsShown] = useState(false);
-  const [isReversed, setIsReversed] = useState(false);
-  const [sortBy, setSortBy] = useState('id');
-
-  const visibleGoods = [...goodsFromServer];
-
-  visibleGoods.sort((a: string, b: string): number => {
-    switch (sortBy) {
-      case 'alphabet': return a.localeCompare(b);
-      case 'length': return a.length - b.length;
-      default: return 0;
-    }
-  });
-
-  if (isReversed) {
-    visibleGoods.reverse();
-  }
 
   return (
     <div className="App content">
       <h1 className="title mt-6">Goods</h1>
-      {!isShown && (
-        <button
-          type="button"
-          className="button is-info is-inverted"
-          onClick={() => setIsShown(true)}
-        >
-          Start
-        </button>
-      )}
-
-      {isShown && (
-        <>
-          <div className="buttons">
-            <button
-              type="button"
-              className="button is-info is-inverted"
-              onClick={() => setIsReversed(current => !current)}
-            >
-              Reverse
-            </button>
-            <button
-              type="button"
-              className="button is-info is-inverted"
-              onClick={() => setSortBy('alphabet')}
-            >
-              Sort alphabetically
-            </button>
-            <button
-              type="button"
-              className="button is-info is-inverted"
-              onClick={() => {
-                setSortBy('id');
-                setIsReversed(false);
-              }}
-            >
-              Reset
-            </button>
-            <button
-              type="button"
-              className="button is-info is-inverted"
-              onClick={() => setSortBy('length')}
-            >
-              Sort by length
-            </button>
-          </div>
-          <GoodsList goods={visibleGoods} />
-        </>
-      )}
+      {!isShown
+        ? (
+          <button
+            type="button"
+            className="button is-info is-inverted"
+            onClick={() => setIsShown(true)}
+          >
+            Start
+          </button>
+        )
+        : <GoodsList goods={goodsFromServer} />}
     </div>
   );
 };
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {
   goods: string[],
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li key={good}>
-        {good}
-      </li>
-    ))}
-  </ul>
-);
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  const visibleGoods = [...goods];
+  const [isReversed, setIsReversed] = useState(false);
+  const [sortBy, setSortBy] = useState('id');
+
+  visibleGoods.sort((a: string, b: string): number => {
+    switch (sortBy) {
+      case 'alphabet': return a.localeCompare(b);
+      case 'length': return a.length - b.length;
+      default: return 0;
+    }
+  });
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return (
+    <>
+      <div className="buttons">
+        <button
+          type="button"
+          className="button is-info is-inverted"
+          onClick={() => setIsReversed(current => !current)}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          className="button is-info is-inverted"
+          onClick={() => setSortBy('alphabet')}
+        >
+          Sort alphabetically
+        </button>
+        <button
+          type="button"
+          className="button is-info is-inverted"
+          onClick={() => {
+            setSortBy('id');
+            setIsReversed(false);
+          }}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          className="button is-info is-inverted"
+          onClick={() => setSortBy('length')}
+        >
+          Sort by length
+        </button>
+      </div>
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good}>
+            {good}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
