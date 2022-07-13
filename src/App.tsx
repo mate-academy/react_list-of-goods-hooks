8e42b448ecde994b7ab35b1c6db56c5015f50e3c
mhrynko <glukym@gmail.com>
2022-07-13 17:57:37 +0300

    fix description and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -14,11 +15,33 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
+export const App: React.FC = () => (
   <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
+    <button type="button">
+      Start
+    </button>
+
+    <button type="button">
+      Sort alphabetically
+    </button>
+
+    <button type="button">
+      Sort by length
+    </button>
+
+    <button type="button">
+      Reverse
+    </button>
+
+    <button type="button">
+      Reset
+    </button>
+
+    <ul className="Goods">
+      <li className="Goods__item">Dumplings</li>
+      <li className="Goods__item">Carrot</li>
+      <li className="Goods__item">Eggs</li>
+      <li className="Goods__item">...</li>
+    </ul>
   </div>
 );
-
-export default App;
