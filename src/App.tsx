57b9b53b6a02804f11a7d21733a37bdcc5bf9d77
Roman Marunkevich <vinnibea@gmail.com>
2022-07-14 07:08:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
@@ -15,33 +15,148 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum Sort {
+  length = 'length',
+  name = 'name',
+  default = 'default',
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+export const App: React.FC = () => {
+  const [visible, isVisible] = useState(true);
+  const [reverse, setReverse] = useState(false);
+  const [switched, switchEffect] = useState(true);
+  const [sort, sortBy] = useState<Sort>(Sort.default);
+  const [reset, resetStatus] = useState(false);
+  const [limit, setLimit] = useState(1);
 
-    <button type="button">
-      Sort by length
-    </button>
+  const products: string[] = [...goodsFromServer]
+    .filter(product => product.length > limit);
 
-    <button type="button">
-      Reverse
-    </button>
+  useEffect(() => {
+    if (reverse) {
+      setReverse(false);
+    }
+  }, [switched]);
 
-    <button type="button">
-      Reset
-    </button>
+  useEffect(() => {
+    sortBy(Sort.default);
+    setReverse(false);
+  }, [reset]);
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  switch (sort) {
+    case Sort.name:
+      products.sort((a, b) => a.localeCompare(b));
+      break;
+
+    case Sort.length:
+      products.sort((a, b) => a.length - b.length);
+      break;
+
+    default:
+      break;
+  }
+
+  if (reverse) {
+    products.reverse();
+  }
+
+  return (
+    <div className="App">
+      {visible
+        ? (
+          <div className="App__buttons-block">
+            <button
+              type="button"
+              className="button"
+              onClick={() => {
+                isVisible(() => !visible);
+              }}
+            >
+              Start
+            </button>
+          </div>
+        )
+        : (
+          <>
+            <div className="App__buttons-block">
+              <button
+                type="button"
+                className="button"
+                onClick={() => {
+                  switchEffect(() => !switched);
+                  sortBy(Sort.name);
+                }}
+              >
+                Sort by name
+              </button>
+
+              <button
+                type="button"
+                className="button"
+                onClick={() => {
+                  switchEffect(() => !switched);
+                  sortBy(Sort.length);
+                }}
+              >
+                Sort by length
+              </button>
+
+              <button
+                className="button"
+                type="button"
+                onClick={() => setReverse(!reverse)}
+              >
+                Reverse
+              </button>
+
+              <button
+                className="button"
+                type="button"
+                onClick={() => resetStatus(!reset)}
+              >
+                Reset
+              </button>
+            </div>
+
+            <ul className="Goods">
+              <h3 className="App__info">
+                {products.length ? `Items on page: ${products.length}` : 'Nothing to show'}
+              </h3>
+              {products.map(product => (
+                <li
+                  className="Goods__item"
+                  key={product}
+                >
+                  {product}
+                </li>
+              ))}
+            </ul>
+
+            <input
+              type="range"
+              defaultValue="0"
+              min="1"
+              max="10"
+              onChange={({ target }) => setLimit(+target.value)}
+            />
+
+            <span>
+              Use arrows to change limit
+            </span>
+
+            <br />
+
+            <span>
+              {`Current words are longer than ${limit} symbol('s)`}
+            </span>
+
+            <datalist id="tickmarks">
+              {products.map((product, index) => (
+                <option value={index + 1} key={product}> </option>
+              ))}
+            </datalist>
+          </>
+        )}
+    </div>
+  );
+};
