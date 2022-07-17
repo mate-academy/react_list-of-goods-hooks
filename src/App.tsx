506268904f84dd71e2cb9e91a4b4b115ac09367b
Stas Yasinchyk <grodh06@gmail.com>
2022-07-17 18:07:57 +0300

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { GoodsList } from './GoodsLIst';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
@@ -15,33 +16,124 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortBy {
+  ALPHABET = 'A-Z',
+  LENGTH = 'Length',
+  NONE = 'none',
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+export const App: React.FC = () => {
+  const goods:string[] = [...goodsFromServer];
+  const [itsBegin, setBegin] = useState(true);
+  const [isReversed, setReversed] = useState(false);
+  const [sortBy, setSortBy] = useState<SortBy>(SortBy.NONE);
+  const [selectedLength, setLength] = useState(1);
 
-    <button type="button">
-      Sort by length
-    </button>
+  const visibleGoods = goods.filter(good => good.length >= selectedLength);
 
-    <button type="button">
-      Reverse
-    </button>
+  const reset = () => {
+    setLength(1);
+    setReversed(false);
+    setSortBy(SortBy.NONE);
+  };
 
-    <button type="button">
-      Reset
-    </button>
+  const selectLenght = (value: number) => {
+    setLength(value);
+  };
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  switch (sortBy) {
+    case SortBy.ALPHABET:
+      visibleGoods.sort((curGood, nextGood) => curGood.localeCompare(nextGood));
+      break;
+
+    case SortBy.LENGTH:
+      visibleGoods
+        .sort((curGood, nextGood) => curGood.length - nextGood.length);
+      break;
+
+    default:
+      break;
+  }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        {itsBegin
+          ? (
+            <button
+              className="button is-success level-item"
+              type="button"
+              onClick={() => setBegin(false)}
+            >
+              Start
+            </button>
+          )
+          : (
+            <div className="message is-primary">
+              <div className="message-header title is-3 level">
+                <h1>Goods</h1>
+                <button
+                  className="button is-warning"
+                  type="button"
+                  onClick={() => reset()}
+                >
+                  Reset
+                </button>
+              </div>
+              <div className="">
+                <div className="container">
+                  <GoodsList goods={visibleGoods} />
+                </div>
+              </div>
+              <div className="buttons-container content level">
+                <button
+                  className="button is-primary"
+                  type="button"
+                  onClick={() => setReversed(!isReversed)}
+                >
+                  Reverse
+                </button>
+                <button
+                  className="button is-primary"
+                  type="button"
+                  onClick={() => setSortBy(SortBy.ALPHABET)}
+                >
+                  Sort by A-Z
+                </button>
+                <button
+                  className="button is-primary"
+                  type="button"
+                  onClick={() => setSortBy(SortBy.LENGTH)}
+                >
+                  Sort by lenght
+                </button>
+                <div className="select is-success">
+                  <select
+                    value={selectedLength}
+                    onChange={({ target }) => {
+                      selectLenght(Number(target.value));
+                    }}
+                  >
+                    {Array.from(Array(11).keys()).map((item) => (
+                      <option
+                        key={item}
+                        value={item}
+                      >
+                        {item}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+
+          )}
+
+      </div>
+    </div>
+  );
+};
