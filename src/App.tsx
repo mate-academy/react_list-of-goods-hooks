ed78468b3efc8743ae2dee91c24fc64485d51f15
Viacheslav Tsymbal <slava.tsymbal@ukr.net>
2022-07-18 17:04:48 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -15,33 +14,75 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+export const App: React.FC = () => {
+  const [listIsVisible, setListVisibility] = useState(false);
+  const [goods, setGoods] = useState([...goodsFromServer]);
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+  return (
+    <div className="App">
+      {listIsVisible === false
+        ? (
+          <button
+            className="App__start"
+            type="button"
+            onClick={() => setListVisibility(true)}
+          >
+            Start
+          </button>
+        ) : (
+          <div className="App__container">
+            <div className="App__container-buttons">
+              <button
+                className="App__container-button"
+                type="button"
+                onClick={() => setGoods([...goodsFromServer]
+                  .sort((good1, good2) => {
+                    return good1.localeCompare(good2);
+                  }))}
+              >
+                Sort alphabetically
+              </button>
 
-    <button type="button">
-      Sort by length
-    </button>
+              <button
+                className="App__container-button"
+                type="button"
+                onClick={() => setGoods([...goodsFromServer]
+                  .sort((good1, good2) => {
+                    return good1.length - good2.length;
+                  }))}
+              >
+                Sort by length
+              </button>
 
-    <button type="button">
-      Reverse
-    </button>
+              <button
+                className="App__container-button"
+                type="button"
+                onClick={() => setGoods([...goodsFromServer].reverse())}
+              >
+                Reverse
+              </button>
 
-    <button type="button">
-      Reset
-    </button>
+              <button
+                className="App__container-button"
+                type="button"
+                onClick={() => setGoods([...goodsFromServer])}
+              >
+                Reset
+              </button>
+            </div>
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+            <ul className="App__goods">
+              {goods.map(good => (
+                <li
+                  className="App__good"
+                  key={good}
+                >
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+    </div>
+  );
+};
