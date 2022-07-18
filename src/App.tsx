7f91b6717d5a3953756291f39cc02698a6577797
SAMAEV <andreysamaev@gmail.com>
2022-07-18 13:35:57 +0300

    goodsHooked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -15,33 +15,121 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+export const App: React.FC = () => {
+  const goods = [...goodsFromServer]
 
-    <button type="button">
+  const [isVisible, setVisio] = useState(false);
+  const [isReversed, setReversed] = useState(false);
+  const [isSortByAlphabet, sortAlphabetically] = useState(false);
+  const [isSortByLength, sortByLength] = useState(false);
+  const [isReset, setResetedGoods] = useState(false);
+  const [goodsLength, setGoodsLength] = useState('1');
+  const visibleGoods = goods.filter(good => good.length >= Number(goodsLength));
+
+  if (isSortByLength) {
+    visibleGoods.sort((good1, good2) => good1.length - good2.length);
+  }
+
+  if (isSortByAlphabet) {
+    visibleGoods.sort((good1, good2) => good1.localeCompare(good2));
+    }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  if (isReset) {
+    setReversed(false);
+    sortAlphabetically(false);
+    sortByLength(false);
+    setResetedGoods(false);
+    setGoodsLength('1');
+  }
+
+return (
+  <div className="App message">
+    {!isVisible && <div>
+      <p
+        className='title message-header'>
+          Control of goods starts from pushing this
+      </p>
+      <button
+        type="button"
+        className='button is-large is-fullwidth'
+        onClick={() =>setVisio(true)}
+      >
+        Start
+    </button>
+    </div>
+    }
+
+{isVisible && <div>
+    <button
+      type="button"
+      className='button is-dark'
+      onClick={() => {
+        sortAlphabetically(true);
+        sortByLength(false);
+        setReversed(false);
+      }}
+    >
       Sort alphabetically
     </button>
 
-    <button type="button">
+    <button
+      type="button"
+      className='button is-dark'
+      onClick={() => {
+        sortByLength(true);
+        sortAlphabetically(false);
+        setReversed(false);
+      }}
+    >
       Sort by length
     </button>
 
-    <button type="button">
+    <button
+      type="button"
+      className='button is-dark'
+      onClick={() => setReversed(!isReversed)}
+    >
       Reverse
     </button>
 
-    <button type="button">
+    <button
+      type="button"
+      className='button is-dark'
+      onClick={() => setResetedGoods(true)}
+    >
       Reset
     </button>
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
+    <ul className="Goods ">
+      {visibleGoods.map(good =>(
+        <li
+          className="Goods__item"
+          key={good}>{good}
+        </li>
+      ))}
     </ul>
+
+    <div>
+      <p className='message-body'>You can choose items to start length from value:</p>
+      <select name="select" value={goodsLength} onChange={({ target }) => setGoodsLength(target.value)}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    </div>}
   </div>
-);
+  );
+};
