16ef6b4e97fba5e1b309069494696b29f83938b4
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-19 14:52:10 +0300

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -36,86 +36,93 @@ export const App: React.FC = () => {
     goods.reverse();
   }
 
+  const btnSortPress = () => {
+    sortByLength(false);
+    sortByAlph(!isSortedByAlph);
+  };
+
+  const btnSort2Press = () => {
+    sortByAlph(false);
+    sortByLength(!isSortedByLength);
+  };
+
+  const btnResetPress = () => {
+    sortByLength(false);
+    sortByAlph(false);
+    reverse(false);
+    filterByLength(1);
+  };
+
   return (
     <div className="App">
       <h1 className="title">Goods</h1>
-      {isStarted ? (
-        <>
-          <GoodsList goods={goods} />
+      {isStarted
+        ? (
+          <>
+            <GoodsList goods={goods} />
 
-          <div className="select is-success">
-            <select
-              name="length"
-              value={minLength}
-              onChange={(event) => filterByLength(+event.target.value)}
+            <div className="select is-success">
+              <select
+                name="length"
+                value={minLength}
+                onChange={(event) => filterByLength(+event.target.value)}
+              >
+                {[...new Array(10)].map((_, i) => (
+                  <option value={i + 1}>{i + 1}</option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              type="button"
+              onClick={btnSortPress}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isSortedByAlph },
+              )}
             >
-              {[...new Array(10)].map((_, i) => (
-                <option value={i + 1}>{i + 1}</option>
-              ))}
-            </select>
-          </div>
+              Sort alphabetically
+            </button>
 
+            <button
+              type="button"
+              onClick={btnSort2Press}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isSortedByLength },
+              )}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={() => reverse(!isReversed)}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isReversed },
+              )}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={btnResetPress}
+              className="button is-danger"
+            >
+              Reset
+            </button>
+          </>
+        ) : (
           <button
             type="button"
-            onClick={() => {
-              sortByLength(false);
-              sortByAlph(!isSortedByAlph);
-            }}
-            className={classNames(
-              'button is-info',
-              { 'is-success': isSortedByAlph },
-            )}
+            onClick={() => start(true)}
+            className="button is-success"
           >
-            Sort alphabetically
+            Start
           </button>
-
-          <button
-            type="button"
-            onClick={() => {
-              sortByAlph(false);
-              sortByLength(!isSortedByLength);
-            }}
-            className={classNames(
-              'button is-info',
-              { 'is-success': isSortedByLength },
-            )}
-          >
-            Sort by length
-          </button>
-
-          <button
-            type="button"
-            onClick={() => reverse(!isReversed)}
-            className={classNames(
-              'button is-info',
-              { 'is-success': isReversed },
-            )}
-          >
-            Reverse
-          </button>
-
-          <button
-            type="button"
-            onClick={() => {
-              sortByLength(false);
-              sortByAlph(false);
-              reverse(false);
-              filterByLength(1);
-            }}
-            className="button is-danger"
-          >
-            Reset
-          </button>
-        </>
-      ) : (
-        <button
-          type="button"
-          onClick={() => start(true)}
-          className="button is-success"
-        >
-          Start
-        </button>
-      )}
+        )}
     </div>
   );
 };
