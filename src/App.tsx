9680b16e678d018493adf05a2f90735945265dce
Kate Khon <katerynakhon@gmail.com>
2022-07-20 10:05:22 +0300

    commit after m review

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,14 @@ function getReorderedGoods(
     element.length >= minLength
   ));
 
-  if (sortType === SortType.ALPABET) {
-    visibleGoods.sort((good1, good2) => good1.localeCompare(good2));
-  } else if (sortType === SortType.LENGTH) {
-    visibleGoods.sort((good1, good2) => good1.length - good2.length);
+  switch (sortType) {
+    case SortType.ALPABET:
+      visibleGoods.sort((good1, good2) => good1.localeCompare(good2));
+      break;
+    case SortType.LENGTH:
+      visibleGoods.sort((good1, good2) => good1.length - good2.length);
+      break;
+    default:
   }
 
   return isReversed ? visibleGoods.reverse() : visibleGoods;
@@ -45,100 +49,100 @@ export const App: React.FC = () => {
   const [sortType, setSortType] = useState(SortType.NONE);
   const [lengthValue, setLengthValue] = useState(1);
 
+  const handleEvent = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setLengthValue(+event.target.value);
+  };
+
+  const reorderedGoods = getReorderedGoods(
+    goodsFromServer,
+    sortType,
+    isReversed,
+    lengthValue,
+  );
+
   return (
     <div className="App box has-background-warning-light">
-      {!isStarted && (
-        <button
-          type="button"
-          onClick={() => {
-            setIsStarted(true);
-          }}
-          className="button is-success is-light"
-        >
-          Start
-        </button>
-      )}
-
-      {isStarted && (
-        <>
-          <div className="buttons">
-            <button
-              type="button"
-              onClick={() => {
-                setSortType(SortType.ALPABET);
-              }}
-              className="button is-info is-light"
-            >
-              Sort alphabetically
-            </button>
-
-            <button
-              type="button"
-              onClick={() => {
-                setSortType(SortType.LENGTH);
-              }}
-              className="button is-info is-light"
-            >
-              Sort by length
-            </button>
-
-            <button
-              type="button"
-              onClick={() => {
-                setIsReversed(!isReversed);
-              }}
-              className="button is-info is-light"
-            >
-              Reverse
-            </button>
-
-            <button
-              type="button"
-              onClick={() => {
-                setIsReversed(false);
-                setSortType(SortType.NONE);
-                setLengthValue(1);
-              }}
-              className="button is-danger is-light"
-            >
-              Reset
-            </button>
-          </div>
-
-          <ul className="Goods content is-large block">
-            {getReorderedGoods(
-              goodsFromServer,
-              sortType,
-              isReversed,
-              lengthValue,
-            ).map(product => (
-              <li
-                className="Goods__item"
-                key={product}
-              >
-                {product}
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <p className="text">Select min length of word:</p>
-
-            <div className="select is-multiply is-success block">
-              <select
-                name="minWordLength"
-                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                  setLengthValue(+event.target.value);
+      {!isStarted
+        ? (
+          <button
+            type="button"
+            onClick={() => {
+              setIsStarted(true);
+            }}
+            className="button is-success is-light"
+          >
+            Start
+          </button>
+        ) : (
+          <>
+            <div className="buttons">
+              <button
+                type="button"
+                onClick={() => {
+                  setSortType(SortType.ALPABET);
                 }}
+                className="button is-info is-light"
               >
-                {[...Array(10)].map((_, i) => (
-                  <option value={i + 1}>{i + 1}</option>
-                ))}
-              </select>
+                Sort alphabetically
+              </button>
+
+              <button
+                type="button"
+                onClick={() => {
+                  setSortType(SortType.LENGTH);
+                }}
+                className="button is-info is-light"
+              >
+                Sort by length
+              </button>
+
+              <button
+                type="button"
+                onClick={() => {
+                  setIsReversed(!isReversed);
+                }}
+                className="button is-info is-light"
+              >
+                Reverse
+              </button>
+
+              <button
+                type="button"
+                onClick={() => {
+                  setIsReversed(false);
+                  setSortType(SortType.NONE);
+                  setLengthValue(1);
+                }}
+                className="button is-danger is-light"
+              >
+                Reset
+              </button>
             </div>
-          </div>
-        </>
-      )}
+
+            <ul className="Goods content is-large block">
+              {reorderedGoods.map(product => (
+                <li
+                  className="Goods__item"
+                  key={product}
+                >
+                  {product}
+                </li>
+              ))}
+            </ul>
+
+            <div>
+              <p className="text">Select min length of word:</p>
+
+              <div className="select is-multiply is-success block">
+                <select name="minWordLength" onChange={handleEvent}>
+                  {[...Array(10)].map((_, i) => (
+                    <option value={i + 1}>{i + 1}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </>
+        )}
     </div>
   );
 };
