8a4efe4627c3e468917a9681b15072d44a216841
Misha Dzhemula <medvedmisha23@gmail.com>
2022-08-08 16:24:34 +0300

    replaced options with arr

--- src/App.tsx
+++ src/App.tsx
@@ -116,16 +116,9 @@ export const App: React.FC = () => {
               value={minLength}
               onChange={e => setMinLength(+e.currentTarget.value)}
             >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => (
+                  <option value={item}>{item}</option>
+                ))}
             </select>
           </label>
 
