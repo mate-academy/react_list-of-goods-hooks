aff02b0dfd3cf9019195cb0141b75cfdb6e97f27
Oleg Vetrov <oleg94038@gmail.com>
2022-08-09 17:26:46 +0300

    Added App component with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import classNames from 'classnames';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
@@ -15,33 +16,110 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE,
+  ALPHABET,
+  LENGTH,
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+// eslint-disable-next-line max-len
+function getReorderedGoods(goods: string[], sortType: SortType, isReversed: boolean) {
+  const visibleGoods = [...goods].sort((a, b) => {
+    switch (sortType) {
+      case SortType.ALPHABET:
+        return a.localeCompare(b);
+      case SortType.LENGTH:
+        return a.length - b.length;
+      default:
+        return 0;
+    }
+  });
 
-    <button type="button">
-      Sort by length
-    </button>
+  if (isReversed) {
+    return visibleGoods.reverse();
+  }
 
-    <button type="button">
-      Reverse
-    </button>
+  return visibleGoods;
+}
 
-    <button type="button">
-      Reset
-    </button>
+export const App: React.FC = () => {
+  const [showGoods, setShowGoods] = useState(false);
+  const [sortType, setSortType] = useState(0);
+  const [reverse, setReverse] = useState(false);
+  const [activeButton, setActiveButton] = useState('');
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  const preparedGoods
+    = getReorderedGoods(goodsFromServer, sortType, reverse);
+
+  return (
+    <div className="App">
+      {(!showGoods
+        && (
+          <button
+            type="button"
+            className="button"
+            onClick={() => (
+              setShowGoods(true)
+            )}
+          >
+            Start
+          </button>
+        )) || (
+        <>
+          <div className="buttons">
+            <button
+              type="button"
+              className={classNames('button',
+                { active: activeButton === 'alpha' })}
+              onClick={() => {
+                setSortType(1);
+                setActiveButton('alpha');
+              }}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              className={classNames('button',
+                { active: activeButton === 'length' })}
+              onClick={() => {
+                setSortType(2);
+                setActiveButton('length');
+              }}
+            >
+              Sort by length
+            </button>
+            <button
+              type="button"
+              className={classNames('button',
+                { active: activeButton === 'reverse' })}
+              onClick={() => {
+                setReverse(!reverse);
+                setActiveButton('reverse');
+              }}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              className={classNames('button',
+                { active: activeButton === 'reset' })}
+              onClick={() => {
+                setSortType(0);
+                setReverse(false);
+                setActiveButton('reset');
+              }}
+            >
+              Reset
+            </button>
+          </div>
+          <ul className="Goods">
+            {preparedGoods.map(good => (
+              <li key={good} className="Goods__item">{good}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
