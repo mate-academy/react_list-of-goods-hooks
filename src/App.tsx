52ae562fd4f80ffeb40089ac7b66d5c71cfaba80
PavloMarkov <r9markovpn@gmail.com>
2022-08-11 18:06:29 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -15,33 +14,151 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE,
+  ALPABET,
+  LENGTH,
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+export const App: React.FC = () => {
+  const [isStarted, setStarted] = useState(false);
+  const showGoods = () => {
+    setStarted(true);
+  };
 
-    <button type="button">
-      Sort by length
-    </button>
+  const [sortMethod, setSortMethod] = useState(0);
 
-    <button type="button">
-      Reverse
-    </button>
+  const alphabetSort = () => {
+    setSortMethod(SortType.ALPABET);
+  };
 
-    <button type="button">
-      Reset
-    </button>
+  const lengthSort = () => {
+    setSortMethod(SortType.LENGTH);
+  };
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  const [isReverse, setReverseMethod] = useState(false);
+  const reverseSort = () => {
+    setReverseMethod(current => !current);
+  };
+
+  const defaultSort = () => {
+    setSortMethod(SortType.NONE);
+    setReverseMethod(false);
+  };
+
+  const [minLength, setMinLength] = useState(1);
+  const minLengthSort = (n:number) => {
+    setMinLength(n);
+  };
+
+  function neededGoods(arr: string[]) {
+    const copy = [...arr];
+
+    copy.sort((a, b) => {
+      switch (sortMethod) {
+        case 1:
+          return a.localeCompare(b);
+
+        case 2:
+          return a.length - b.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReverse) {
+      copy.reverse();
+    }
+
+    return copy.filter(good => good.length >= minLength);
+  }
+
+  const goods = neededGoods(goodsFromServer);
+
+  return (
+    <div className="App">
+      {!isStarted && (
+        <button
+          type="button"
+          className="button is-success is-rounded is-fullwidth"
+          onClick={showGoods}
+        >
+          Start
+        </button>
+      )}
+
+      {isStarted && (
+        <>
+          <button
+            type="button"
+            className={`button is-rounded ${sortMethod === SortType.ALPABET ? 'is-success' : ''}`}
+            onClick={alphabetSort}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            className={`button is-rounded ${sortMethod === SortType.LENGTH ? 'is-success' : ''}`}
+            onClick={lengthSort}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className={`button is-rounded ${isReverse ? 'is-success' : ''}`}
+            onClick={reverseSort}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            className="button is-rounded is-danger"
+            onClick={defaultSort}
+          >
+            Reset
+          </button>
+
+          <ul className="Goods">
+            {goods.map(good => (
+              <li className="Goods__item" key={good}>
+                {good}
+              </li>
+            ))}
+          </ul>
+
+          <div className="length">
+            Select minimal length of word to sort:
+          </div>
+          <div
+            className="select"
+          >
+            <select
+              name="maxLength"
+              id="maxLength"
+              value={minLength}
+              onChange={(event) => minLengthSort(+event.currentTarget.value)}
+            >
+              {Array(10).fill(1).map((num, i) => {
+                const count = num + i;
+
+                return (
+                  <option
+                    value={count}
+                    key={count}
+                  >
+                    {count}
+                  </option>
+                );
+              })}
+
+            </select>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
