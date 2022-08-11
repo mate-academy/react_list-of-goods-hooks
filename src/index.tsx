75d6824054b4c9718fb916899220317aca82eb62
Olha Stakhova <olha.stakhova@gmail.com>
2022-08-11 15:56:14 +0300

    fixed bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+enum SortType {
+  NONE,
+  ALPABET,
+  LENGTH,
+}
+
 ReactDOM.render(
-  <App />,
+  <App
+    isStarted={false}
+    isReversed={false}
+    sortType={SortType.NONE}
+    value={0}
+  />,
   document.getElementById('root'),
 );
