170fd6a9a47d15cee174cebfa14e7fe491c60812
Olha Stakhova <olha.stakhova@gmail.com>
2022-08-11 15:45:13 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
@@ -15,33 +16,153 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE,
+  ALPABET,
+  LENGTH,
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+type State = {
+  isStarted: boolean,
+  isReversed: boolean,
+  sortType: SortType,
+  value: number,
+};
 
-    <button type="button">
-      Sort by length
-    </button>
+export const App: React.FC<State> = () => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [sortType, setSortType] = useState(SortType.NONE);
+  const [isReversed, setIsReversed] = useState(false);
+  const [value, setValue] = useState(1);
 
-    <button type="button">
-      Reverse
-    </button>
+  const start = () => {
+    setIsStarted(true);
+  };
 
-    <button type="button">
-      Reset
-    </button>
+  const sortByAlpabet = () => {
+    setSortType(SortType.ALPABET);
+  };
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  const sortByLength = () => {
+    setSortType(SortType.LENGTH);
+  };
+
+  const reverse = () => {
+    setIsReversed(!isReversed);
+  };
+
+  const reset = () => {
+    setSortType(SortType.NONE);
+    setIsReversed(false);
+    setValue(1);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setValue(Number(event.target.value));
+  };
+
+  const getReorderedGoods = (
+    goods: string[],
+    sortBy: SortType,
+    isRevers: boolean,
+    valueSelect: number,
+  ): string[] => {
+    const visibleGoods = goods
+      .filter(good => good.length >= valueSelect);
+
+    if (sortBy === SortType.ALPABET) {
+      visibleGoods.sort((a, b) => b.localeCompare(a));
+    }
+
+    if (sortBy === SortType.LENGTH) {
+      visibleGoods.sort((a, b) => b.length - a.length);
+    }
+
+    if (!isRevers) {
+      visibleGoods.reverse();
+    }
+
+    return visibleGoods;
+  };
+
+  return (
+    <div className="App">
+      {!isStarted && (
+        <button
+          type="button"
+          className="button is-warning"
+          onClick={() => start()}
+        >
+          Start
+        </button>
+      )}
+
+      {isStarted
+      && (
+        <>
+          <div className="button-wrapper">
+            <button
+              type="button"
+              className="button is-success"
+              onClick={() => sortByAlpabet()}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              className="button is-success"
+              onClick={() => sortByLength()}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              className="button is-success"
+              onClick={() => reverse()}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              className="button is-success"
+              onClick={() => reset()}
+            >
+              Reset
+            </button>
+          </div>
+
+          <ul className="Goods">
+            { getReorderedGoods(
+              goodsFromServer,
+              sortType,
+              isReversed,
+              value,
+            ).map(good => (
+              <li className="Goods__item" key={good}>{good}</li>
+            ))}
+          </ul>
+          <span className="titleSelect">Filterd by name length:</span>
+          <select
+            name="select"
+            value={value}
+            onChange={handleChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </>
+      )}
+    </div>
+  );
+};
