55d9c90fd746c885575e223b5fcce6b52e875043
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-08-11 20:46:10 +0300

    task is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, ButtonGroup } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goodsFromServer: string[] = [
@@ -15,33 +18,127 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE = 'noSort',
+  ALPHABET = 'alphabetSort',
+  LENGTH = 'lengthSort',
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+function getReorderedGoods(
+  goods: string[],
+  sortType: SortType,
+  isReversed: boolean,
+) {
+  // Not to mutate the original array
+  const visibleGoods = [...goods];
 
-    <button type="button">
-      Sort by length
-    </button>
+  visibleGoods.sort((a, b) => {
+    switch (sortType) {
+      case SortType.ALPHABET:
+        return a.localeCompare(b);
 
-    <button type="button">
-      Reverse
-    </button>
+      case SortType.LENGTH:
+        return a.length - b.length;
 
-    <button type="button">
-      Reset
-    </button>
+      default:
+        return 0;
+    }
+  });
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  if (isReversed) {
+    return visibleGoods.reverse();
+  }
+
+  return visibleGoods;
+}
+
+export const App: React.FC = () => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [isReversed, setIsReversed] = useState(false);
+  const [sortType, setSortType] = useState(SortType.NONE);
+
+  const handleStart = () => (setIsStarted(true));
+  const handleSortAlphabetically = () => (setSortType(SortType.ALPHABET));
+  const handleSortLength = () => (setSortType(SortType.LENGTH));
+  const handleReverse = () => (setIsReversed(value => !value));
+
+  const handleReset = () => {
+    setIsStarted(false);
+    setIsReversed(false);
+    setSortType(SortType.NONE);
+  };
+
+  const reversedGoods = getReorderedGoods(
+    goodsFromServer,
+    sortType,
+    isReversed,
+  );
+
+  return (
+    <div className="App">
+      {!isStarted && (
+        <Button
+          type="button"
+          onClick={handleStart}
+        >
+          Start
+        </Button>
+      )}
+
+      {isStarted && (
+        <>
+          <ButtonGroup
+            variant="outlined"
+            aria-label="outlined button group"
+          >
+            <Button
+              type="button"
+              onClick={handleSortAlphabetically}
+              variant={sortType === 'alphabetSort'
+                ? 'contained'
+                : 'outlined'}
+            >
+              Sort alphabetically
+            </Button>
+            <Button
+              type="button"
+              onClick={handleSortLength}
+              variant={sortType === 'lengthSort'
+                ? 'contained'
+                : 'outlined'}
+            >
+              Sort by length
+            </Button>
+            <Button
+              type="button"
+              onClick={handleReverse}
+              variant={isReversed === true
+                ? 'contained'
+                : 'outlined'}
+            >
+              Reverse
+            </Button>
+            <Button
+              type="button"
+              onClick={handleReset}
+              color="error"
+            >
+              Reset
+            </Button>
+          </ButtonGroup>
+
+          <List className="Goods">
+            {reversedGoods.map(good => (
+              <ListItem
+                className="Goods__item"
+                key={good}
+              >
+                {good}
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
+    </div>
+  );
+};
