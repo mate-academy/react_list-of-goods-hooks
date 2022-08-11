a70da39209682c4fb181b90b7b8025460431a431
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-08-11 19:52:58 +0300

    refactored to simple hooks

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,16 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
+interface Good {
+  name: string;
+  id: string;
+}
+
+const formattedGoodList: Good[] = goodsFromServer.map((good) => ({
+  name: good,
+  id: v4(),
+}));
+
 enum SortType {
   NONE,
   ALPABET,
@@ -25,7 +35,7 @@ enum SortType {
 }
 
 function getReorderedGoods(
-  goods: string[],
+  goods: Good[],
   sortType: SortType,
   isReversed: boolean,
 ) {
@@ -35,10 +45,10 @@ function getReorderedGoods(
   // Sort and reverse goods if needed
   switch (sortType) {
     case SortType.ALPABET:
-      visibleGoods.sort((a, b) => a.localeCompare(b));
+      visibleGoods.sort((a, b) => a.name.localeCompare(b.name));
       break;
     case SortType.LENGTH:
-      visibleGoods.sort((a, b) => a.length - b.length);
+      visibleGoods.sort((a, b) => a.name.length - b.name.length);
       break;
     case SortType.NONE:
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
@@ -52,80 +62,60 @@ function getReorderedGoods(
   return isReversed ? visibleGoods.reverse() : visibleGoods;
 }
 
-interface State {
-  isStarted: boolean;
-  isReversed: boolean;
-  sortType: SortType;
-}
-
-const initialState = {
-  isStarted: false,
-  isReversed: false,
-  sortType: SortType.NONE,
-};
-
 export const App: React.FC = () => {
-  const [{ isStarted, isReversed, sortType }, setState]
-    = useState<State>(initialState);
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+  const [isReversed, setIsReversed] = useState<boolean>(false);
+  const [sortType, setSortType] = useState<SortType>(SortType.NONE);
 
-  const start = () =>
-    setState((prevState) => ({ ...prevState, isStarted: true }));
+  const start = () => setIsStarted(true);
 
-  const reset = () => setState({ ...initialState, isStarted: true });
+  const reset = () => {
+    setIsReversed(false);
+    setSortType(SortType.NONE);
+  };
 
-  const reverse = () =>
-    setState((prevState) => ({
-      ...prevState,
-      isReversed: !isReversed,
-    }));
+  const reverse = () => setIsReversed(!isReversed);
 
-  const sortByAlphabet = () =>
-    setState((prevState) => ({
-      ...prevState,
-      sortType: SortType.ALPABET,
-    }));
+  const sortByAlphabet = () => setSortType(SortType.ALPABET);
 
-  const sortByLength = () =>
-    setState((prevState) => ({
-      ...prevState,
-      sortType: SortType.LENGTH,
-    }));
+  const sortByLength = () => setSortType(SortType.LENGTH);
 
-  const goodList = getReorderedGoods(goodsFromServer, sortType, isReversed);
-
-  const formattedGoodList = goodList.map((good) => ({
-    name: good,
-    id: v4(),
-  }));
-
-  const buttons = [
-    { value: 'Sort alphabetically', handler: sortByAlphabet },
-    { value: 'Sort by length', handler: sortByLength },
-    { value: 'Reverse', handler: reverse },
-    { value: 'Reset', handler: reset },
-  ].map((button) => ({
-    ...button,
-    id: v4(),
-  }));
-
-  const buttonListToRender = buttons.map(({ value, handler, id }) => (
-    <button
-      key={id}
-      onClick={handler}
-      className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      type="button"
-    >
-      {value}
-    </button>
-  ));
+  const goodList = getReorderedGoods(formattedGoodList, sortType, isReversed);
 
   return (
     <div className="App font-mono bg-stone-800 h-screen">
       {isStarted ? (
         <div className="container mx-auto p-35 text-center">
-          {buttonListToRender}
+          <button
+            onClick={sortByAlphabet}
+            className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            type="button"
+          >
+            Sort alphabetically
+          </button>
+          <button
+            onClick={sortByLength}
+            className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            type="button"
+          >
+            Sort by length
+          </button>
+          <button
+            onClick={reverse}
+            className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            type="button"
+          >
+            Reverse
+          </button>
+          <button
+            onClick={reset}
+            className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            type="button"
+          >
+            Reset
+          </button>
           <ul className="Goods space-y-35">
-            {formattedGoodList.map(({ name, id }) => (
+            {goodList.map(({ name, id }) => (
               <li
                 key={id}
                 className="Goods__item hover:underline text-white text-xl"
