659bd5123382e38000dcda001469210933c016a8
Oleksìi <bardyshev@gmail.com>
2022-08-12 16:25:30 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './App.css';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const goodsFromServer: string[] = [
+const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,33 +15,124 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE = 'None',
+  ALPHABET = 'Alphabet',
+  LENGTH = 'length',
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+function getReorderedGoods(
+  goods: string[],
+  sortType: SortType,
+  isReversed: boolean,
+) {
+  const visibleGoods = [...goods];
 
-    <button type="button">
-      Sort by length
-    </button>
+  visibleGoods.sort((good1, good2) => {
+    switch (sortType) {
+      case SortType.ALPHABET:
+        return good1.localeCompare(good2);
 
-    <button type="button">
-      Reverse
-    </button>
+      case SortType.LENGTH:
+        return good1.length - good2.length;
 
-    <button type="button">
-      Reset
-    </button>
+      default:
+        return 0;
+    }
+  });
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return visibleGoods;
+}
+
+export const App: React.FC = () => {
+  const [isStarted, setStart] = useState(false);
+  const [isReversed, setReversed] = useState(false);
+  const [sortType, setSortType] = useState(SortType.NONE);
+
+  const visibleGoods = getReorderedGoods(
+    goodsFromServer,
+    sortType,
+    isReversed,
+  );
+
+  return (
+    <div className="App">
+      {!isStarted
+        ? (
+          <button
+            type="button"
+            onClick={() => setStart(true)}
+            className="btn btn-success"
+          >
+            Start
+          </button>
+        ) : (
+          <>
+            <div className="buttons">
+              <button
+                type="button"
+                onClick={() => setSortType(SortType.ALPHABET)}
+                className={classNames(
+                  'btn', {
+                    'btn-outline-success': sortType !== SortType.ALPHABET,
+                  }, {
+                    'btn-success': sortType === SortType.ALPHABET,
+                  },
+                )}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                type="button"
+                onClick={() => setSortType(SortType.LENGTH)}
+                className={classNames(
+                  'btn', {
+                    'btn-outline-success': sortType !== SortType.LENGTH,
+                  }, {
+                    'btn-success': sortType === SortType.LENGTH,
+                  },
+                )}
+              >
+                Sort by length
+              </button>
+
+              <button
+                type="button"
+                onClick={() => setReversed(prevState => !prevState)}
+                className="btn btn-outline-success"
+              >
+                Reverse
+              </button>
+
+              <button
+                type="button"
+                onClick={() => {
+                  setSortType(SortType.NONE);
+                  setReversed(false);
+                }}
+                className="btn btn-outline-success"
+              >
+                Reset
+              </button>
+            </div>
+
+            <ul className="list-group">
+              {visibleGoods.map(good => (
+                <li
+                  className="list-item list-group-item"
+                  key={good}
+                >
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+    </div>
+  );
+};
