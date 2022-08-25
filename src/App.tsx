b7e8854ed0dd50285510dd0823d00c227f4d7393
Andriy Nosov <nosovandriy@gmail.com>
2022-08-25 23:39:34 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -15,33 +15,108 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-export const App: React.FC = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+enum SortType {
+  NONE,
+  ALPHABET,
+  LENGTH,
+}
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+function getReorderedGoods(
+  goods: string[],
+  sortType: SortType,
+  isReversed: boolean,
+) {
+  const visibleGoods = [...goods];
 
-    <button type="button">
-      Sort by length
-    </button>
+  visibleGoods.sort((g1, g2) => {
+    switch (sortType) {
+      case SortType.ALPHABET:
+        return g1.localeCompare(g2);
+      case SortType.LENGTH:
+        return g1.length - g2.length;
+      default:
+        return 0;
+    }
+  });
 
-    <button type="button">
-      Reverse
-    </button>
+  const reverseResult = isReversed
+    ? visibleGoods.reverse()
+    : visibleGoods;
 
-    <button type="button">
-      Reset
-    </button>
+  return reverseResult;
+}
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+export const App: React.FC = () => {
+  const [isStarted, setIsStarted] = React.useState<boolean>(false);
+  const [isReversed, setIsReversed] = React.useState<boolean>(false);
+  const [sortType, setSortType] = React.useState<SortType>(SortType.NONE);
+
+  const visibleGoods = getReorderedGoods(goodsFromServer, sortType, isReversed);
+
+  const sortByAbc = () => {
+    setIsReversed(false);
+    setSortType(SortType.ALPHABET);
+  };
+
+  const sortByLength = () => {
+    setIsReversed(false);
+    setSortType(SortType.LENGTH);
+  };
+
+  const resetSortReverse = () => {
+    setIsReversed(false);
+    setSortType(SortType.NONE);
+  };
+
+  return (
+    <div className="App">
+      {!isStarted
+        ? (
+          <button
+            type="button"
+            onClick={() => setIsStarted(true)}
+          >
+            Start
+          </button>
+        ) : (
+          <>
+            <button
+              type="button"
+              onClick={sortByAbc}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              onClick={sortByLength}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={() => setIsReversed(!isReversed)}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={resetSortReverse}
+            >
+              Reset
+            </button>
+
+            <ul className="Goods">
+              {visibleGoods.map((good) => (
+                <li key={good}>
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+    </div>
+  );
+};
