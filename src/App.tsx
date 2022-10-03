3b0714d95e323950de5a9ec403a8d1657593a9c9
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-10-03 11:22:35 +0300

    Switch case used

--- src/App.tsx
+++ src/App.tsx
@@ -32,15 +32,16 @@ function getReorderedGoods(
   { sortType, isReversed }: ReorderOptions,
 ) {
   const visibleGoods = [...goods].sort((good1, good2) => {
-    if (sortType === SortType.ALPABET) {
-      return good1.localeCompare(good2);
-    }
+    switch (sortType) {
+      case SortType.ALPABET:
+        return good1.localeCompare(good2);
 
-    if (sortType === SortType.LENGTH) {
-      return good1.length - good2.length;
-    }
+      case SortType.LENGTH:
+        return good1.length - good2.length;
 
-    return 1;
+      default:
+        return 1;
+    }
   });
 
   if (isReversed) {
@@ -54,70 +55,69 @@ export const App: React.FC = () => {
   const [sortType, setSortType] = useState(SortType.NONE);
   const [isReversed, reverse] = useState(false);
 
-  let goods = getReorderedGoods(goodsFromServer, {sortType, isReversed});
+  const goods = getReorderedGoods(goodsFromServer, { sortType, isReversed });
 
   return (
     <div className="section content">
-        <div className="buttons">
-          <button
-            type="button"
-            className={classNames(
-              'button',
-              'is-info',
-              { 'is-light': sortType !== SortType.ALPABET },
-            )}
-            onClick={() => setSortType(SortType.ALPABET)}
-          >
-            Sort alphabetically
-          </button>
+      <div className="buttons">
+        <button
+          type="button"
+          className={classNames(
+            'button',
+            'is-info',
+            { 'is-light': sortType !== SortType.ALPABET },
+          )}
+          onClick={() => setSortType(SortType.ALPABET)}
+        >
+          Sort alphabetically
+        </button>
 
-          <button
-            type="button"
-            className={classNames(
-              'button',
-              'is-success',
-              { 'is-light': sortType !== SortType.LENGTH },
-            )}
-            onClick={() => setSortType(SortType.LENGTH)}
-          >
-            Sort by length
-          </button>
+        <button
+          type="button"
+          className={classNames(
+            'button',
+            'is-success',
+            { 'is-light': sortType !== SortType.LENGTH },
+          )}
+          onClick={() => setSortType(SortType.LENGTH)}
+        >
+          Sort by length
+        </button>
 
-          <button
-            type="button"
-            className={classNames(
-              'button',
-              'is-warning',
-              { 'is-light': !isReversed },
-            )}
-            onClick={() => reverse(!isReversed)}
-          >
-            Reverse
-          </button>
+        <button
+          type="button"
+          className={classNames(
+            'button',
+            'is-warning',
+            { 'is-light': !isReversed },
+          )}
+          onClick={() => reverse(!isReversed)}
+        >
+          Reverse
+        </button>
 
-          {isReversed || sortType !== SortType.NONE
-            ? (
-              <button
-                type="button"
-                className="button is-danger is-light"
-                onClick={() => {
-                  reverse(false);
-                  setSortType(SortType.NONE);
-                }}
-              >
-                Reset
-              </button>
-            )
-            : ('')}
-        </div>
-
-        <ul>
-          <ul>
-            {goods.map(good => (
-              <li key={good} data-cy="Good">{good}</li>
-            ))}
-          </ul>
-        </ul>
+        {(isReversed || sortType !== SortType.NONE)
+          && (
+            <button
+              type="button"
+              className="button is-danger is-light"
+              onClick={() => {
+                reverse(false);
+                setSortType(SortType.NONE);
+              }}
+            >
+              Reset
+            </button>
+          )}
       </div>
+
+      <ul>
+        <ul>
+          {goods.map(good => (
+            <li key={good} data-cy="Good">{good}</li>
+          ))}
+        </ul>
+      </ul>
+    </div>
   );
 };
