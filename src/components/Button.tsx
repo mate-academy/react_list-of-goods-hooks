e57cc0185bfadebf9efbcce64402517a3b9c1645
George.kom <komolovua@gmail.com>
2022-10-24 21:34:43 +0300

    Functional implementation of react list of goods

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+export const Button: React.FC<Props> = (props) => {
+  const {
+    children,
+    className,
+    ...rest
+  } = props;
+
+  return (
+    <button
+      type="button"
+      className={classNames('button', className)}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  reorderedGoods: string[]
+}
+
+export const GoodsList: React.FC<Props> = ({ reorderedGoods }) => (
+  <ul>
+    {reorderedGoods.map(good => (
+      <li key={good} data-cy="Good">
+        { good }
+      </li>
+    ))}
+  </ul>
+);
