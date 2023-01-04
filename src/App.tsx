b382d2406989218957c28eefbca0b5d717cdb170
vadiimvooo <ASD4S.Profile@gmail.com>
2023-01-04 18:02:00 +0200

    Fixed all issues according to review

--- src/App.tsx
+++ src/App.tsx
@@ -34,23 +34,21 @@ function getReorderedGoods(
   const visibleGoods = [...goods];
 
   visibleGoods.sort((firstGood, secondGood) => {
-    if (sortType === SortType.ALPHABET) {
-      return firstGood.localeCompare(secondGood);
-    }
+    switch (sortType) {
+      case SortType.ALPHABET:
+        return firstGood.localeCompare(secondGood);
 
-    if (sortType === SortType.LENGTH) {
-      return Number(firstGood.length) - Number(secondGood.length);
-    }
+      case SortType.LENGTH:
+        return Number(firstGood.length) - Number(secondGood.length);
 
-    return 0;
+      default:
+        return 0;
+    }
   });
 
   if (isReversed) {
     visibleGoods.reverse();
   }
-
-  // eslint-disable-next-line no-console
-  console.log(sortType, isReversed);
 
   return visibleGoods;
 }
