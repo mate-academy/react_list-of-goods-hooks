13542a014abc1b5ed10758aa813e7e7eccf47878
Bogdan <bogdan_mykhailov@icloud.com>
2023-03-19 16:28:46 +0200

    :white_check_mark: implement listOfGoods using hooks

---
+++ src/components/Goods/Goods.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import { createId } from '../../utils/uuid-creator';
+
+interface GoodsProps {
+  goods: string[];
+}
+
+export const Goods: FC<GoodsProps> = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li key={createId()} data-cy="Good">
+          {good}
+        </li>
+      ))}
+    </ul>
+  );
+};
