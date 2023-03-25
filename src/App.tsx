44b7a7e647fe2fa10d2b7e13f873b7afd2719316
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-03-25 11:12:59 +0200

    fixed enum values

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ export const goodsFromServer = [
 ];
 
 enum SortType {
-  NONE = 0,
-  ALPHABET = 1,
-  LENGTH = 2,
+  NONE = 'none',
+  ALPHABET = 'alphabet',
+  LENGTH = 'length',
 }
 
 type ReorderOptions = {
