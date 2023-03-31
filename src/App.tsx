fb93e6818e45f171c6341d5f6606a95caec61aee
Sasha Kononenko <nurchiksasha@gmail.com>
2023-03-31 09:55:32 +0300

    test commit for improving code

--- src/App.tsx
+++ src/App.tsx
@@ -17,31 +17,90 @@ export interface GoodsSortingOptions {
   sortType: SortType;
 }
 
+// This is also test code but I am not syre that is correct
+// export function getReorderedGoods(
+//   goods: string[],
+//   { isReversed = false, sortType }: GoodsSortingOptions,
+// ) {
+//   const visibleGoods = [...goods];
+
+//   visibleGoods.sort((firstGoods, secondGoods) => {
+//     switch (sortType) {
+//       case SortType.ALPHABET:
+//         return (isReversed ? -1 : 1) * firstGoods.localeCompare(secondGoods);
+
+//       case SortType.LENGTH:
+//         return (isReversed ? -1 : 1) * (firstGoods.length - secondGoods.length);
+
+//       default:
+//         return 0;
+//     }
+//   });
+
+//   return visibleGoods;
+// }
+
+// test code
 export function getReorderedGoods(
   goods: string[],
-  { isReversed, sortType }: GoodsSortingOptions,
+  { isReversed = false, sortType }: GoodsSortingOptions,
+  isReverseSorting = false,
 ) {
   const visibleGoods = [...goods];
 
   visibleGoods.sort((firstGoods, secondGoods) => {
+    let order = isReversed ? -1 : 1;
+
     switch (sortType) {
       case SortType.ALPHABET:
-        return firstGoods.localeCompare(secondGoods);
+        order *= firstGoods.localeCompare(secondGoods);
+        break;
 
       case SortType.LENGTH:
-        return (firstGoods.length - secondGoods.length);
+        order *= firstGoods.length - secondGoods.length;
+        break;
 
       default:
-        return 0;
+        order = 0;
+        break;
     }
-  });
 
-  if (isReversed) {
-    visibleGoods.reverse();
-  }
+    if (isReverseSorting) {
+      order *= -1;
+    }
+
+    return order;
+  });
 
   return visibleGoods;
 }
+
+// Valid code!!! dont remove this!!
+// export function getReorderedGoods(
+//   goods: string[],
+//   { isReversed, sortType }: GoodsSortingOptions,
+// ) {
+//   const visibleGoods = [...goods];
+
+//   visibleGoods.sort((firstGoods, secondGoods) => {
+//     switch (sortType) {
+//       case SortType.ALPHABET:
+//         return firstGoods.localeCompare(secondGoods);
+
+//       case SortType.LENGTH:
+//         return (firstGoods.length - secondGoods.length);
+
+//       default:
+//         return 0;
+//     }
+//   });
+
+//   if (isReversed) {
+//     visibleGoods.reverse();
+//   }
+
+//   return visibleGoods;
+// }
 
 export const App: React.FC = () => {
   const [isReversed, setIsReversed] = useState(false);
