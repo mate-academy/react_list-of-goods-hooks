f7a7e02a394a978c2e3cd3f04b286aa0b2567201
Andrii <remote.makandriival@gmail.com>
2023-04-21 13:01:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,34 +16,127 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  NONE,
+  ALPHABET,
+  LENGTH,
+}
+
+type ReorderOptions = {
+  sortType: SortType,
+  isReversed: boolean,
+};
+
+export function getReorderedGoods(
+  goods: string[],
+  { sortType, isReversed }: ReorderOptions,
+) {
+  const visibleGoods = [...goods];
+
+  switch (sortType) {
+    case SortType.LENGTH:
+      visibleGoods.sort((a, b) => a.length - b.length);
+      break;
+
+    case SortType.ALPHABET:
+      visibleGoods.sort((a, b) => a.localeCompare(b));
+      break;
+
+    case SortType.NONE:
+      break;
+
+    default:
+      throw new Error('something went wrong');
+  }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(sortType, isReversed);
+
+  return visibleGoods;
+}
+
 export const App: React.FC = () => {
+  const [isReversed, setIsReserved] = useState(false);
+  const [sortType, setSortType] = useState(SortType.NONE);
+
+  const sortByAlphabet = () => {
+    setSortType(SortType.ALPHABET);
+  };
+
+  const sortByLength = () => {
+    setSortType(SortType.LENGTH);
+  };
+
+  const toggleReverse = () => {
+    setIsReserved(!isReversed);
+  };
+
+  const reset = () => {
+    setIsReserved(false);
+    setSortType(SortType.NONE);
+  };
+
+  const goods = getReorderedGoods(goodsFromServer, {
+    sortType,
+    isReversed,
+  });
+
+  const sortAphabetClassName = classNames('button is-info', {
+    'is-light': sortType !== SortType.ALPHABET,
+  });
+
+  const sortLengthClassName = classNames('button is-success', {
+    'is-light': sortType !== SortType.LENGTH,
+  });
+
+  const reverseClassName = classNames(
+    'button is-warning',
+    { 'is-light': !isReversed },
+  );
+
+  const resetClassName = classNames(
+    'button is-danger is-light',
+    {
+      'is-hidden':
+      goodsFromServer[1] === goods[1],
+    },
+  );
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={sortAphabetClassName}
+          onClick={sortByAlphabet}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={sortLengthClassName}
+          onClick={sortByLength}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={reverseClassName}
+          onClick={toggleReverse}
         >
           Reverse
         </button>
 
         <button
           type="button"
-          className="button is-danger is-light"
+          className={resetClassName}
+          onClick={reset}
         >
           Reset
         </button>
@@ -50,12 +144,9 @@ export const App: React.FC = () => {
 
       <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {goods.map(good => (
+            <li key={good} data-cy="Good">{good}</li>
+          ))}
         </ul>
       </ul>
     </div>
