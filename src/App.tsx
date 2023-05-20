89173827f95b90ef087feed23a78da9ce9a1f04e
Nazar Kharkevych <nkharkevych@gmail.com>
2023-05-20 14:14:40 +0300

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -53,24 +54,16 @@ export const App: React.FC = () => {
 
   const showResetButton = sortType !== SortType.NONE || isReversed;
 
-  const sortedAlphabetically = sortType === SortType.ALPHABET
-    ? ''
-    : 'is-light';
-
-  const sortedByLength = sortType === SortType.LENGTH
-    ? ''
-    : 'is-light';
-
-  const reversed = isReversed
-    ? ''
-    : 'is-light';
-
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${sortedAlphabetically}`}
+          className={classNames(
+            'button',
+            'is-info',
+            { 'is-light': sortType !== SortType.ALPHABET },
+          )}
           onClick={() => setSortType(SortType.ALPHABET)}
         >
           Sort alphabetically
@@ -78,7 +71,11 @@ export const App: React.FC = () => {
 
         <button
           type="button"
-          className={`button is-success ${sortedByLength}`}
+          className={classNames(
+            'button',
+            'is-success',
+            { 'is-light': sortType !== SortType.LENGTH },
+          )}
           onClick={() => setSortType(SortType.LENGTH)}
         >
           Sort by length
@@ -86,7 +83,11 @@ export const App: React.FC = () => {
 
         <button
           type="button"
-          className={`button is-warning ${reversed}`}
+          className={classNames(
+            'button',
+            'is-warning',
+            { 'is-light': !isReversed },
+          )}
           onClick={() => setIsReversed(!isReversed)}
         >
           Reverse
