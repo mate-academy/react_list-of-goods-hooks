778af2eba006c82b0d7d32affcb50d54357ecda9
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-06-05 21:32:39 +0300

    list of good with hooks was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -16,12 +16,56 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
+  const [isReversed, setIsReversed] = useState(false);
+  const [sortedBy, setSortedBy] = useState('none');
+  const [isVisible, setIsVisible] = useState(false);
+
+  const changeArray = (array: string[]) => {
+    const visibleGoods = [...array];
+
+    switch (sortedBy) {
+      case 'none':
+        break;
+
+      case 'alphabet':
+        visibleGoods.sort((good1, good2) => {
+          return good1.localeCompare(good2);
+        });
+        break;
+
+      case 'length':
+        visibleGoods.sort((good1, good2) => {
+          return good1.length - good2.length;
+        });
+        break;
+
+      default:
+        break;
+    }
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    return visibleGoods;
+  };
+
+  const newGoods = changeArray(goodsFromServer);
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
           className="button is-info is-light"
+          onClick={(e) => {
+            setSortedBy('alphabet');
+            setIsVisible(true);
+            e.currentTarget.classList.remove('is-light');
+
+            return document
+              .querySelector('.is-success')?.classList.add('is-light');
+          }}
         >
           Sort alphabetically
         </button>
@@ -29,6 +73,14 @@ export const App: React.FC = () => {
         <button
           type="button"
           className="button is-success is-light"
+          onClick={(e) => {
+            setSortedBy('length');
+            setIsVisible(true);
+            e.currentTarget.classList.remove('is-light');
+
+            return document
+              .querySelector('.is-info')?.classList.add('is-light');
+          }}
         >
           Sort by length
         </button>
@@ -36,26 +88,48 @@ export const App: React.FC = () => {
         <button
           type="button"
           className="button is-warning is-light"
+          onClick={(e) => {
+            setIsReversed(!isReversed);
+
+            if (isReversed && Array.from(
+              document.querySelectorAll('is-light'),
+            ).length < 1) {
+              setIsVisible(false);
+            } else {
+              setIsVisible(true);
+            }
+
+            return isReversed
+              ? e.currentTarget.classList.add('is-light')
+              : e.currentTarget.classList.remove('is-light');
+          }}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {isVisible && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setIsVisible(false);
+              setSortedBy('none');
+              setIsReversed(false);
+
+              Array.from(document.querySelectorAll('button'))
+                .forEach(buttom => buttom.classList.add('is-light'));
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {newGoods.map(good => (
+            <li data-cy="Good" key={good}>{good}</li>
+          ))}
         </ul>
       </ul>
     </div>
