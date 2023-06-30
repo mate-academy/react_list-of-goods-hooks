120a41b927639f980b15fb67182567023adedc1c
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-30 18:14:15 +0300

    code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-export const goodsFromServer = [
+const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,48 +17,127 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Alphabet = 'alphabet',
+  Length = 'length',
+  Default = '',
+}
+
+const getNewSort = (
+  goods: string[],
+  sortType: SortType,
+  reversed: boolean,
+): string[] => {
+  const newGoods = [...goods];
+
+  if (!sortType && reversed) {
+    return newGoods.reverse();
+  }
+
+  if (sortType) {
+    newGoods.sort((good1, good2) => {
+      switch (sortType) {
+        case SortType.Length:
+          if (good1.length !== good2.length) {
+            return reversed
+              ? good2.length - good1.length
+              : good1.length - good2.length;
+          }
+
+          return reversed
+            ? good2.localeCompare(good1)
+            : good1.localeCompare(good2);
+
+        case SortType.Alphabet:
+          return reversed
+            ? good2.localeCompare(good1)
+            : good1.localeCompare(good2);
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return newGoods;
+};
+
 export const App: React.FC = () => {
+  const [sortType, setSortType] = useState<SortType>(SortType.Default);
+  const [sortReverse, setSortReverse] = useState(false);
+  const updateGoods = getNewSort(goodsFromServer, sortType, sortReverse);
+
+  const newSortType = (type: SortType) => {
+    setSortType(type);
+  };
+
+  const newSortReverse = () => {
+    setSortReverse(!sortReverse);
+  };
+
+  const buttonClasses = classNames('button', 'is-info', {
+    'is-light': sortType !== SortType.Alphabet,
+  });
+
+  const sortByLength = classNames('button', 'is-info', 'is-success', {
+    'is-light': sortType !== SortType.Length,
+  });
+
+  const reverseClasses = classNames('button', 'is-info', 'is-warning', {
+    'is-light': !sortReverse,
+  });
+
+  const resetClasses = classNames('button', 'is-danger', 'is-light', {
+    hidden: sortType === SortType.Default && !sortReverse,
+  });
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={buttonClasses}
+          onClick={() => newSortType(SortType.Alphabet)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={sortByLength}
+          onClick={() => newSortType(SortType.Length)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={reverseClasses}
+          onClick={newSortReverse}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {(sortType !== SortType.Default || sortReverse) && (
+          <button
+            type="button"
+            className={resetClasses}
+            onClick={() => {
+              setSortType(SortType.Default);
+              setSortReverse(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {updateGoods.map((good) => (
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
