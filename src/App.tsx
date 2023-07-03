3252bb39f29b2677d372c9ddf476fbef059915d8
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-07-03 13:21:11 +0300

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ export const App: React.FC = () => {
           Reverse
         </button>
 
-        {(sortType !== 0 || isReversed) && (
+        {(sortType !== SortType.NONE || isReversed) && (
           <button
             type="button"
             className="button is-danger is-light"
