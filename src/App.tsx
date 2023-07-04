f43bc286607555a45bb7c3c3a8e3a511b1d75e96
Antonio S <soi4.toxa@ukr.net>
2023-07-04 15:54:19 +0300

    Developed-by-Antonio--1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+
+import React, { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,48 +16,116 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+export enum SortType {
+  Default = '',
+  Name = 'name',
+  Lenght = 'length',
+}
+
+function getPreparedGoods(
+  goodsFrom: string[],
+  sortField: SortType,
+  hasReverse: boolean,
+) {
+  const goods = [...goodsFrom];
+
+  if (sortField) {
+    goods.sort((goodA, goodB) => {
+      switch (sortField) {
+        case SortType.Name:
+          return goodA.localeCompare(goodB);
+
+        case SortType.Lenght:
+          return goodA.length - goodB.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (hasReverse) {
+    goods.reverse();
+  }
+
+  return goods;
+}
+
+function getResetValue(
+  sortField: SortType,
+  hasReverse: boolean,
+) {
+  if (sortField.length > 0 || hasReverse) {
+    return true;
+  }
+
+  return false;
+}
+
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState<SortType>(SortType.Default);
+  const [hasReverse, setHasReverse] = useState(false);
+
+  const hasReset = getResetValue(sortField, hasReverse);
+  const sortListGoods = getPreparedGoods(
+    goodsFromServer,
+    sortField,
+    hasReverse,
+  );
+
+  const deleteSort = () => {
+    setHasReverse(false);
+    setSortField(SortType.Default);
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={sortField === SortType.Name
+            ? 'button is-info'
+            : 'button is-info is-light'}
+          onClick={() => setSortField(SortType.Name)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={sortField === SortType.Lenght
+            ? 'button is-info'
+            : 'button is-info is-light'}
+          onClick={() => setSortField(SortType.Lenght)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={hasReverse
+            ? 'button is-info'
+            : 'button is-info is-light'}
+          onClick={() => setHasReverse(!hasReverse)}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {hasReset && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={deleteSort}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {sortListGoods.map(good => (
+          <li key={good} data-cy="Good">{good}</li>
+        ))}
       </ul>
     </div>
   );
