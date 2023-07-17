16ae32f9198fe6e04b55e4bb118b09d134aec967
Dmytro Somik <dmytro.somik@mccann.com>
2023-07-17 16:43:42 +0100

    Solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,48 +16,112 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Default = 0,
+  Name = 1,
+  Length = 2,
+}
+
+type Filter = {
+  sortField: SortType,
+  isSorted: boolean,
+};
+
+function sortGoods(goods: string[], { sortField, isSorted }: Filter) {
+  const localGoods = [...goods];
+
+  if (sortField) {
+    localGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SortType.Name:
+          return good1.localeCompare(good2);
+
+        case SortType.Length:
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (isSorted) {
+    localGoods.reverse();
+  }
+
+  return localGoods;
+}
+
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState<SortType>(SortType.Default);
+  const [isSorted, setSorted] = useState(false);
+
+  const makeSetSortField = (field: SortType) => () => setSortField(field);
+  const makeSetSorted = (sorted: boolean) => () => setSorted(sorted);
+
+  const showGoods = sortGoods(
+    goodsFromServer,
+    { sortField, isSorted },
+  );
+
+  const reset = () => {
+    setSortField(SortType.Default);
+    setSorted(false);
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn(
+            ['button', 'is-info'],
+            { 'is-light': sortField !== SortType.Name },
+          )}
+          onClick={makeSetSortField(SortType.Name)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn(
+            ['button', 'is-success'],
+            { 'is-light': sortField !== SortType.Length },
+          )}
+          onClick={makeSetSortField(SortType.Length)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn(
+            ['button', 'is-warning'],
+            {
+              'is-light': !isSorted,
+            },
+          )}
+          onClick={makeSetSorted(!isSorted)}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {(sortField || isSorted) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {showGoods.map(good => (
+          <li data-cy="Good" key={good}>{good}</li>
+        ))}
       </ul>
     </div>
   );
