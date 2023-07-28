3936bcf015280bad34162c8570ccca6cf06f9b95
Vitalii Lola <lola1v41@gmail.com>
2023-07-28 12:05:13 +0300

    Solution

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 > DON'T use class components (for this and next tasks).
 
-Reimplement the **React list of goods** implemented before using hooks and typescript.
-Copy everything you need from your previous solution.
+Повторный список ** реагировать список товаров **, внедренных перед использованием крючков и типовой,.
+Скопируйте все, что вам нужно, из вашего предыдущего решения.
 
 > Here is [the working version](https://mate-academy.github.io/react_list-of-goods/)
 
@@ -14,4 +14,4 @@ Copy everything you need from your previous solution.
 
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_list-of-goods-hooks/) and add it to the PR description.
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://g37ua.github.io/react_list-of-goods-hooks/) and add it to the PR description.
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-export const goodsFromServer = [
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,48 +17,115 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
+enum SortField {
+  string = 'alphabetically',
+  num = 'length',
+}
+
+interface GoodsProps {
+  sortField: SortField | '';
+  reversed: boolean;
+}
+
+function getPreparedGoods(
+  goods: string[],
+  { sortField, reversed }: GoodsProps,
+): string[] {
+  const preparedGoods = [...goods];
+
+  if (sortField) {
+    preparedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SortField.string:
+          return good1.localeCompare(good2);
+
+        case SortField.num:
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (reversed) {
+    preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
+export const App = () => {
+  const [sortField, setSortField] = useState<SortField | ''>('');
+  const [reversed, setReversed] = useState<boolean>(false);
+
+  const visibleGoods = getPreparedGoods(
+    goodsFromServer,
+    {
+      sortField,
+      reversed,
+    },
+  );
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn(
+            'button',
+            'is-info',
+            { 'is-light': sortField !== SortField.string },
+          )}
+          onClick={() => setSortField(SortField.string)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn(
+            'button',
+            'is-success',
+            { 'is-light': sortField !== SortField.num },
+          )}
+          onClick={() => setSortField(SortField.num)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn(
+            'button',
+            'is-warning',
+            { 'is-light': !reversed },
+          )}
+          onClick={() => setReversed(!reversed)}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {(sortField || reversed) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setReversed(false);
+              setSortField('');
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visibleGoods.map(good => (
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
