835ed02b85a3dd71f02f0d2a85310955229e56fb
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-08-31 22:40:59 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -16,47 +18,108 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
+  const [goodsArr, setGoodsArr] = useState(goodsFromServer);
+  const [sortValue, setSortValue] = useState('');
+  const [reverseValue, setReverseValue] = useState(false);
+  const [resetValue, setResetValue] = useState(false);
+
+  const SORT_BY_ALPHABETICALLY = 'alphabetically';
+  const SORT_BY_LENGTH = 'length';
+
+  const sortBy = (value: string) => {
+    if (value === SORT_BY_ALPHABETICALLY) {
+      if (reverseValue) {
+        setGoodsArr(prevState => [...prevState].sort((a, b) => b.localeCompare(a)));
+      } else {
+        setGoodsArr(prevState => [...prevState].sort((a, b) => a.localeCompare(b)));
+      }
+
+      setSortValue(SORT_BY_ALPHABETICALLY);
+      setResetValue(true);
+
+      return;
+    }
+
+    if (value === SORT_BY_LENGTH) {
+      if (reverseValue) {
+        setGoodsArr(prevState => [...prevState].sort((a, b) => b.length - a.length));
+      } else {
+        setGoodsArr(prevState => [...prevState].sort((a, b) => a.length - b.length));
+      }
+
+      setSortValue(SORT_BY_LENGTH);
+      setResetValue(true);
+
+      return;
+    }
+
+    setGoodsArr(goodsFromServer);
+    setReverseValue(false);
+    setResetValue(false);
+    setSortValue('');
+  };
+
+  const reverse = () => {
+    if (!sortValue) {
+      setResetValue(!resetValue);
+    } else {
+      setResetValue(true);
+    }
+
+    setGoodsArr(prevState => [...prevState].reverse());
+    setReverseValue(!reverseValue);
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn('button is-info', {
+            'is-light': sortValue !== SORT_BY_ALPHABETICALLY,
+          })}
+          onClick={() => sortBy(SORT_BY_ALPHABETICALLY)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn('button is-success', {
+            'is-light': sortValue !== SORT_BY_LENGTH,
+          })}
+          onClick={() => sortBy(SORT_BY_LENGTH)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn('button is-warning', {
+            'is-light': !reverseValue,
+          })}
+          onClick={reverse}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {resetValue && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => sortBy('')}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {goodsArr.map(good => (
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
