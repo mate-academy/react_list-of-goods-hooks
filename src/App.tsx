622b14ecbe8adb871cbc5f71de0c545c0db9ea66
OlegRak86 <rakolegit@gmail.com>
2023-08-31 17:30:47 +0300

    react_list-of-goods-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import cn from 'classnames';
+import { EnumSort } from './Types/EnumSort';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,48 +17,85 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+function sortWithRevers(goods:string[], typeSort:EnumSort, reverse:boolean) {
+  let cloneGoods = [...goods];
+
+  switch (typeSort) {
+    case EnumSort.ALFABET_SORT:
+      cloneGoods.sort((a, b) => (a.localeCompare(b)));
+      break;
+    case EnumSort.WORD_LENGTH_SORT:
+      cloneGoods.sort((a, b) => (a.length - b.length));
+      break;
+    default:
+      cloneGoods = [...goods];
+      break;
+  }
+
+  if (reverse) {
+    cloneGoods.reverse();
+  }
+
+  return cloneGoods;
+}
+
 export const App: React.FC = () => {
+  const [typeSort, setSort] = useState(EnumSort.DEFAULT);
+  const [isReverse, setIsReverse] = useState(false);
+  const visibleGoods = sortWithRevers(goodsFromServer, typeSort, isReverse);
+
+  const reversList = () => (
+    isReverse === false
+      ? setIsReverse(true)
+      : setIsReverse(false)
+  );
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn('button', 'is-info',
+            { 'is-light': typeSort !== EnumSort.ALFABET_SORT })}
+          onClick={() => setSort(EnumSort.ALFABET_SORT)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn('button', 'is-success',
+            { 'is-light': typeSort !== EnumSort.WORD_LENGTH_SORT })}
+          onClick={() => setSort(EnumSort.WORD_LENGTH_SORT)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn('button', 'is-warning', { 'is-light': !isReverse })}
+          onClick={() => reversList()}
+
         >
           Reverse
         </button>
+        {typeSort.length > 0 || isReverse
+          ? (
+            <button
+              type="button"
+              className="button is-danger is-light"
+              onClick={() => {
+                setSort(EnumSort.DEFAULT);
+                setIsReverse(false);
+              }}
+            >
+              Reset
+            </button>
+          ) : ''}
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
       </div>
-
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visibleGoods.map(good => <li key={good} data-cy="Good">{good}</li>)}
       </ul>
     </div>
   );
---
+++ src/Types/EnumSort.ts
@@ -0,0 +1,5 @@
+export enum EnumSort {
+  ALFABET_SORT = 'alfabet',
+  WORD_LENGTH_SORT = 'wordlength',
+  DEFAULT = '',
+}
