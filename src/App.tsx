6692dda0dc7f5eb752943af9d9e79459de4ab04b
AndrMar1939 <advo737@gmail.com>
2023-09-05 15:30:59 +0300

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,48 +17,123 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Idle = 'idle',
+  Alphabet = 'alphabet',
+  Length = 'length',
+}
+
+interface Filter {
+  active: SortType;
+  isReversed: boolean;
+}
+
+const initialFilter: Filter = {
+  active: SortType.Idle,
+  isReversed: false,
+};
+
+const getGoodsForRender = ({ active, isReversed }: Filter): string[] => {
+  const cloneGoodsFromServer = [...goodsFromServer];
+
+  switch (active) {
+    case SortType.Idle:
+      break;
+
+    case SortType.Alphabet:
+      cloneGoodsFromServer.sort((a, b) => a.localeCompare(b));
+      break;
+
+    case SortType.Length:
+      cloneGoodsFromServer.sort((a, b) => a.length - b.length);
+      break;
+
+    default:
+      throw new Error('Switch case error!');
+  }
+
+  if (isReversed) {
+    cloneGoodsFromServer.reverse();
+  }
+
+  return cloneGoodsFromServer;
+};
+
 export const App: React.FC = () => {
+  const [activeFilter, setActiveFilter] = useState(initialFilter);
+
+  const isResetButtonVisible = activeFilter.active !== initialFilter.active
+    || activeFilter.isReversed !== initialFilter.isReversed;
+
+  const handleSetSortFilter = (filter: SortType) => {
+    setActiveFilter({
+      ...activeFilter,
+      active: filter,
+    });
+  };
+
+  const goodsForRender = getGoodsForRender(activeFilter);
+
+  const handleToggleReversed = () => {
+    setActiveFilter({
+      ...activeFilter,
+      isReversed: !activeFilter.isReversed,
+    });
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn('button is-info', {
+            'is-light': activeFilter.active !== SortType.Alphabet,
+          })}
+          onClick={() => handleSetSortFilter(SortType.Alphabet)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn('button is-success', {
+            'is-light': activeFilter.active !== SortType.Length,
+          })}
+          onClick={() => handleSetSortFilter(SortType.Length)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn('button is-warning', {
+            'is-light': !activeFilter.isReversed,
+          })}
+          onClick={handleToggleReversed}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {isResetButtonVisible && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setActiveFilter(initialFilter)}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {goodsForRender.map(good => (
+          <li
+            data-cy="Good"
+            key={good}
+          >
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
