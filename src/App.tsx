3636fb434fb40a16338d19c3a8daf0752053dd7f
Nazar Votkanych <nazarvotkanych@gmail.com>
2023-09-05 16:45:56 +0300

    added typescript typification for ReactListOfGoods

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { GoodList } from './Components/GoodList/GoodList';
+import { goodsFromServer } from './Components/GoodsFromServer';
+import { SortType } from './types/SortType';
 
-export const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+function getPreparedGoods(
+  goods: string[],
+  sortField: SortType,
+  isReversed: boolean,
+) {
+  const preparedGoods: string[] = [...goods];
+
+  if (sortField) {
+    let sortFunction;
+
+    switch (sortField) {
+      case SortType.Name:
+        sortFunction = (
+          good1: string,
+          good2: string,
+        ) => good1.localeCompare(good2);
+        break;
+
+      case SortType.Length:
+        sortFunction = (
+          good1: string,
+          good2: string,
+        ) => good1.length - good2.length;
+        break;
+
+      default:
+        throw new Error(`Unknown sort field: ${sortField}`);
+    }
+
+    if (sortFunction) {
+      preparedGoods.sort(sortFunction);
+    }
+  }
+
+  if (isReversed) {
+    preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
 
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState(SortType.Default);
+  const [isReversed, setIsReversed] = useState(false);
+  const hasSortFieldOrIsReversed = (sortField || isReversed);
+  const visibleGoods = getPreparedGoods(
+    goodsFromServer,
+    sortField,
+    isReversed,
+  );
+
+  function reset(): void {
+    setSortField(SortType.Default);
+    setIsReversed(false);
+  }
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
+          onClick={() => setSortField(SortType.Name)}
           type="button"
-          className="button is-info is-light"
+          className={cn('button', 'is-info', {
+            'is-light': sortField !== SortType.Name,
+          })}
         >
           Sort alphabetically
         </button>
 
         <button
+          onClick={() => setSortField(SortType.Length)}
           type="button"
-          className="button is-success is-light"
+          className={cn('button', 'is-success', {
+            'is-light': sortField !== SortType.Length,
+          })}
         >
           Sort by length
         </button>
 
         <button
+          onClick={() => setIsReversed(!isReversed)}
           type="button"
-          className="button is-warning is-light"
+          className={cn('button', 'is-warning', {
+            'is-light': !isReversed,
+          })}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {hasSortFieldOrIsReversed && (
+          <button
+            onClick={reset}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
       </div>
 
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodList goods={visibleGoods} />
     </div>
   );
 };
---
+++ src/types/SortType.ts
@@ -0,0 +1,5 @@
+export enum SortType {
+  Name = 'name',
+  Length = 'length',
+  Default = '',
+}
