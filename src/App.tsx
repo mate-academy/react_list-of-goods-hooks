2918ae7e9e4d0742b59beb75681849d13c5b2361
Dmytro Hrynchyi <desevoker@gmail.com>
2023-09-06 00:20:14 +0300

    add initial solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import cn from 'classnames';
+import React, { useState } from 'react';
 
-export const goodsFromServer = [
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,48 +16,127 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  NoSort,
+  Name,
+  Length,
+}
+
+enum DisplayOrder {
+  Normal,
+  Reverse,
+}
+
+type DisplayOptions = {
+  sortBy: SortType;
+  displayOrder: DisplayOrder;
+};
+
+function getPreparedGoods(
+  goods: string[],
+  { sortBy, displayOrder }: DisplayOptions,
+) {
+  const preparedGoods = [...goods];
+
+  if (sortBy !== SortType.NoSort) {
+    preparedGoods.sort((a, b) => {
+      switch (sortBy) {
+        case SortType.Name:
+          return a.localeCompare(b);
+
+        case SortType.Length:
+          return a.length - b.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (displayOrder === DisplayOrder.Reverse) {
+    preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
 export const App: React.FC = () => {
+  const [
+    sortBy,
+    setSortBy,
+  ] = useState<SortType>(SortType.NoSort);
+
+  const [
+    displayOrder,
+    setDisplayOrder,
+  ] = useState<DisplayOrder>(DisplayOrder.Normal);
+
+  const toggleDisplayOrder = () => {
+    setDisplayOrder(
+      displayOrder === DisplayOrder.Normal
+        ? DisplayOrder.Reverse
+        : DisplayOrder.Normal,
+    );
+  };
+
+  const resetDisplayOptions = () => {
+    setSortBy(SortType.NoSort);
+    setDisplayOrder(DisplayOrder.Normal);
+  };
+
+  const isOptionsEnabled = sortBy !== SortType.NoSort
+                           || displayOrder !== DisplayOrder.Normal;
+
+  const goods = getPreparedGoods(goodsFromServer, { sortBy, displayOrder });
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={cn('button', 'is-info', {
+            'is-light': sortBy !== SortType.Name,
+          })}
+          onClick={() => setSortBy(SortType.Name)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={cn('button', 'is-success', {
+            'is-light': sortBy !== SortType.Length,
+          })}
+          onClick={() => setSortBy(SortType.Length)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={cn('button', 'is-warning', {
+            'is-light': displayOrder !== DisplayOrder.Reverse,
+          })}
+          onClick={toggleDisplayOrder}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {isOptionsEnabled && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={resetDisplayOptions}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {goods.map(good => (
+          <li data-cy="Good" key={good}>{good}</li>
+        ))}
       </ul>
     </div>
   );
