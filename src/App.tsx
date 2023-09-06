4a3b50166d72ab30d67209315b98b454563cbd70
Tamara Chernetska <cty2802@gmail.com>
2023-09-06 11:22:08 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,47 +16,130 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Default = '',
+  Alphabetically = 'name',
+  ByLength = 'length',
+}
+
+enum SortOrder {
+  Default = 0,
+  Ascending = 1,
+  Descending = -1,
+}
+
+function getPreparedGoods(
+  goods: string[],
+  sortField: SortType,
+  isReversed: SortOrder,
+): string[] {
+  const preparedGoods: string[] = [...goods];
+
+  if (sortField !== SortType.Default) {
+    preparedGoods.sort((firstItem, secondItem) => {
+      if (sortField === SortType.Alphabetically) {
+        return firstItem.localeCompare(secondItem);
+      }
+
+      if (sortField === SortType.ByLength) {
+        return firstItem.length - secondItem.length;
+      }
+
+      return 0;
+    });
+  }
+
+  if (isReversed === SortOrder.Ascending) {
+    preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState(SortType.Default);
+  const [isReversed, setIsReversed] = useState(SortOrder.Default);
+  const isResetButtonVisible = sortField !== SortType.Default
+    || isReversed === SortOrder.Ascending;
+
+  const visibleGoods: string[]
+    = getPreparedGoods(goodsFromServer, sortField, isReversed);
+
+  function setSortReverse(): void {
+    setSortField(SortType.Default);
+    setIsReversed(SortOrder.Default);
+  }
+
+  function changeAscDesc(value: SortOrder): SortOrder {
+    let typeOfOrder = value;
+
+    if (typeOfOrder === SortOrder.Ascending) {
+      typeOfOrder = SortOrder.Descending;
+    } else {
+      typeOfOrder = SortOrder.Ascending;
+    }
+
+    return typeOfOrder;
+  }
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={classNames(
+            'button is-info',
+            { 'is-light': sortField !== SortType.Alphabetically },
+          )}
+          onClick={() => setSortField(SortType.Alphabetically)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={classNames(
+            'button is-success',
+            { 'is-light': sortField !== SortType.ByLength },
+          )}
+          onClick={() => setSortField(SortType.ByLength)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={classNames('button is-warning',
+            { 'is-light': isReversed !== SortOrder.Ascending })}
+          onClick={() => setIsReversed(changeAscDesc(isReversed))}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {isResetButtonVisible && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setSortReverse()}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {visibleGoods.map((item) => {
+            return (
+              <li
+                className="li"
+                data-cy="Good"
+                key={`${item} item`}
+              >
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </ul>
     </div>
