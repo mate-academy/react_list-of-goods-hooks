d5d1f8319604bb67276d892d2b90920261b60221
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-11-22 21:19:44 +0200

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-export const goodsFromServer = [
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -15,48 +17,125 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  length = 'length',
+  alphabet = 'alphabet',
+  default = 0,
+}
+
+function getSortedGoods(
+  goods: string[],
+  sortParameter: SortType,
+  reverseParameter: number,
+): string[] {
+  const copyGoods = [...goods];
+
+  if (reverseParameter === 1) {
+    copyGoods.reverse();
+  }
+
+  if (sortParameter) {
+    copyGoods.sort((good1, good2) => {
+      switch (sortParameter) {
+        case SortType.length:
+          if (reverseParameter) {
+            return good2.length - good1.length;
+          }
+
+          return good1.length - good2.length;
+
+        case SortType.alphabet:
+          if (reverseParameter) {
+            return good2.localeCompare(good1);
+          }
+
+          return good1.localeCompare(good2);
+
+        default:
+          return SortType.default;
+      }
+    });
+  }
+
+  return copyGoods;
+}
+
 export const App: React.FC = () => {
+  const [reverseParameter, setReverseParameter] = useState(0);
+  const [sortParameter, setSortParameter] = useState(SortType.default);
+
+  const visibleGoods
+    = getSortedGoods(goodsFromServer, sortParameter, reverseParameter);
+
+  const reset = () => {
+    setSortParameter(SortType.default);
+    setReverseParameter(0);
+  };
+
+  const reverse = () => {
+    if (reverseParameter) {
+      setReverseParameter(0);
+    }
+
+    if (!reverseParameter) {
+      setReverseParameter(1);
+    }
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
+          onClick={() => setSortParameter(SortType.alphabet)}
           type="button"
-          className="button is-info is-light"
+          className={cn('button is-info', {
+            'is-light': sortParameter !== SortType.alphabet,
+          })}
         >
           Sort alphabetically
         </button>
 
         <button
+          onClick={() => setSortParameter(SortType.length)}
           type="button"
-          className="button is-success is-light"
+          className={cn('button is-success', {
+            'is-light': sortParameter !== SortType.length,
+          })}
         >
           Sort by length
         </button>
 
         <button
+          onClick={reverse}
           type="button"
-          className="button is-warning is-light"
+          className={cn('button is-warning', {
+            'is-light': reverseParameter === 0,
+          })}
         >
           Reverse
         </button>
-
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {(sortParameter || reverseParameter) && (
+          <>
+            <button
+              onClick={reset}
+              type="button"
+              className="button is-danger is-light"
+            >
+              Reset
+            </button>
+          </>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visibleGoods.map(good => (
+          <li
+            data-cy="Good"
+            key={good}
+          >
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
