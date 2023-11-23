31d3edbfb0937b109123e6128c7706e33ec98a9e
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2023-11-23 21:54:21 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,11 @@ enum SortType {
 function getSortedGoods(
   goods: string[],
   sortParameter: SortType,
-  reverseParameter: boolean,
+  isReversed: boolean,
 ): string[] {
   const copyGoods = [...goods];
 
-  if (reverseParameter) {
+  if (isReversed) {
     copyGoods.reverse();
   }
 
@@ -38,18 +38,12 @@ function getSortedGoods(
     copyGoods.sort((good1, good2) => {
       switch (sortParameter) {
         case SortType.length:
-          if (reverseParameter) {
-            return good2.length - good1.length;
-          }
-
-          return good1.length - good2.length;
+          return isReversed ? good2.length - good1.length
+            : good1.length - good2.length;
 
         case SortType.alphabet:
-          if (reverseParameter) {
-            return good2.localeCompare(good1);
-          }
-
-          return good1.localeCompare(good2);
+          return isReversed ? good2.localeCompare(good1)
+            : good1.localeCompare(good2);
 
         default:
           return SortType.default;
@@ -61,32 +55,30 @@ function getSortedGoods(
 }
 
 export const App: React.FC = () => {
-  const [reverseParameter, setReverseParameter] = useState(false);
+  const [isReversed, setIsReversed] = useState(false);
   const [sortParameter, setSortParameter] = useState(SortType.default);
 
   const visibleGoods
-    = getSortedGoods(goodsFromServer, sortParameter, reverseParameter);
+    = getSortedGoods(goodsFromServer, sortParameter, isReversed);
 
   const reset = () => {
     setSortParameter(SortType.default);
-    setReverseParameter(false);
+    setIsReversed(false);
   };
 
   const reverse = () => {
-    if (reverseParameter) {
-      setReverseParameter(false);
-    }
+    setIsReversed(prev => !prev);
+  };
 
-    if (!reverseParameter) {
-      setReverseParameter(true);
-    }
+  const handleSortClick = (type: SortType) => () => {
+    setSortParameter(type);
   };
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
-          onClick={() => setSortParameter(SortType.alphabet)}
+          onClick={handleSortClick(SortType.alphabet)}
           type="button"
           className={cn('button is-info', {
             'is-light': sortParameter !== SortType.alphabet,
@@ -96,7 +88,7 @@ export const App: React.FC = () => {
         </button>
 
         <button
-          onClick={() => setSortParameter(SortType.length)}
+          onClick={handleSortClick(SortType.length)}
           type="button"
           className={cn('button is-success', {
             'is-light': sortParameter !== SortType.length,
@@ -109,12 +101,12 @@ export const App: React.FC = () => {
           onClick={reverse}
           type="button"
           className={cn('button is-warning', {
-            'is-light': reverseParameter === false,
+            'is-light': !isReversed,
           })}
         >
           Reverse
         </button>
-        {(sortParameter || reverseParameter) && (
+        {(sortParameter || isReversed) && (
           <>
             <button
               onClick={reset}
