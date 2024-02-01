54500e3c59bdb571ee95766bced2449678f8b88a
BeginerAlex <aleksander9677@gmail.com>
2024-02-01 19:55:10 +0100

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,48 +17,113 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  SORT_BY_ALPHABET = 'Sort alphabetically',
+  SORT_BY_LENGTH = 'Sort by length',
+}
+
+interface SortParams {
+  sortField: SortType;
+  reverse: boolean;
+}
+
+function getPrepareGoods(goods: string[], { sortField, reverse }: SortParams) {
+  let prepareGoods = [...goods];
+
+  if (sortField) {
+    prepareGoods = prepareGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SortType.SORT_BY_ALPHABET:
+          return good1.localeCompare(good2);
+        case SortType.SORT_BY_LENGTH:
+          return good1.length - good2.length;
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (reverse) {
+    return prepareGoods.reverse();
+  }
+
+  return prepareGoods;
+}
+
 export const App: React.FC = () => {
+  const [sortParams, setSortParams] = useState<SortParams>({
+    sortField: SortType.SORT_BY_ALPHABET,
+    reverse: false,
+  });
+
+  const resetSorting = () => {
+    if (sortParams.sortField || sortParams.reverse) {
+      setSortParams({
+        sortField: SortType.SORT_BY_ALPHABET,
+        reverse: false,
+      });
+    }
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
+          onClick={() => {
+            setSortParams({ sortField: SortType.SORT_BY_ALPHABET, reverse: false });
+          }}
           type="button"
-          className="button is-info is-light"
+          className={cn('button is-info',
+            { 'is-light': sortParams.sortField !== SortType.SORT_BY_ALPHABET })
+          }
         >
           Sort alphabetically
         </button>
 
         <button
+          onClick={() => {
+            setSortParams({ sortField: SortType.SORT_BY_LENGTH, reverse: false});
+          }}
           type="button"
-          className="button is-success is-light"
+          className={cn('button is-success',
+            { 'is-light': sortParams.sortField !== SortType.SORT_BY_LENGTH })
+          }
         >
           Sort by length
         </button>
 
         <button
+          onClick={() => {
+            setSortParams({ ...sortParams, reverse: !sortParams.reverse });
+          }}
           type="button"
-          className="button is-warning is-light"
+          className={cn('button is-warning',
+            { 'is-light': !sortParams.reverse })
+          }
         >
           Reverse
         </button>
 
         <button
+          onClick={resetSorting}
           type="button"
-          className="button is-danger is-light"
+          className={cn('button is-danger is-light',
+            { 'is-hidden': !(sortParams.sortField || sortParams.reverse) })
+          }
         >
           Reset
         </button>
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {getPrepareGoods(goodsFromServer, sortParams).map(good => (
+          <li
+            data-cy="Good"
+            key={good}
+          >
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
