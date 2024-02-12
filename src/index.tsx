8d1c7de2ed9c3941cff369c00e9cea56d107d292
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2024-02-12 18:27:04 +0200

    change index.tsx, add react.strickmode

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
