bf01ca4d4f7692951046927ad7175c1c9e6d5ca1
Eater228 <543145yura@gmail.com>
2024-02-14 18:46:26 +0200

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,116 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
-import './App.scss';
+import React, { useState } from "react";
+import "bulma/css/bulma.css";
+import "./App.scss";
+import cn from "classnames";
 
 export const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
+  "Dumplings",
+  "Carrot",
+  "Eggs",
+  "Ice cream",
+  "Apple",
+  "Bread",
+  "Fish",
+  "Honey",
+  "Jam",
+  "Garlic",
 ];
 
+enum SortType {
+  none = "",
+  alphabet = "alphabet",
+  length = "length",
+}
+
+function getPrepare(arr: string[], sortField: SortType, reverseField: boolean) {
+  const prepareArr = [...arr];
+
+  if (sortField) {
+    prepareArr.sort((good1, good2) => {
+      switch (sortField) {
+        case SortType.alphabet:
+          return good1.localeCompare(good2);
+
+        case SortType.length:
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (reverseField) {
+    prepareArr.reverse();
+  }
+
+  return prepareArr;
+}
+
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState(SortType.none);
+  const [reverseField, setReverseField] = useState(false);
+
+  const visebl = getPrepare(goodsFromServer, sortField, reverseField);
+
+  const reset = () => {
+    setSortField(SortType.none);
+    setReverseField(false);
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          // className="button is-info is-light"
+          className={cn("button is-info", {
+            "is-light": sortField !== SortType.alphabet,
+          })}
+          onClick={() => setSortField(SortType.alphabet)}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          // className="button is-success is-light"
+          className={cn("button is-success", {
+            "is-light": sortField !== SortType.length,
+          })}
+          onClick={() => setSortField(SortType.length)}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          // className="button is-warning is-light"
+          className={cn("button is-warning", {
+            "is-light": reverseField === false,
+          })}
+          onClick={() => setReverseField((current) => !current)}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {(sortField || reverseField) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visebl.map((element) => (
+          <li data-cy="Good" key={element}>
+            {element}
+          </li>
+        ))}
       </ul>
     </div>
   );
