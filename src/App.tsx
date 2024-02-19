750c24832181429d5159643653237713bb21a6c6
krzychu92 <krzysztof.cichy1@gmail.com>
2024-02-19 16:29:08 +0100

    Correct some stuff in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import classNames from "classnames";
 import React, { useState } from "react";
+import classNames from "classnames";
 import "bulma/css/bulma.css";
 import "./App.scss";
+
+enum SortType {
+  NONE,
+  ALPHABET,
+  LENGTH,
+}
 
 export const goodsFromServer = [
   "Dumplings",
@@ -18,17 +24,18 @@ export const goodsFromServer = [
 
 export function getReorderedGoods(
   goods: string[],
-  sortType: string,
+  sortType: SortType,
   isReversed: boolean,
-) {
+): string[] {
   const visibleGoods = [...goods];
 
   visibleGoods.sort((itemOne: string, itemTwo: string): number => {
     switch (sortType) {
-      case "ALPHABET":
+      case SortType.ALPHABET:
         return itemOne.localeCompare(itemTwo);
-      case "LENGTH":
+      case SortType.LENGTH:
         return itemOne.length - itemTwo.length;
+      case SortType.NONE:
       default:
         return 0;
     }
@@ -40,10 +47,17 @@ export function getReorderedGoods(
   return visibleGoods;
 }
 
+
+
 export const App: React.FC = () => {
-  const [sortType, setSortType] = useState("NONE");
+  const [sortType, setSortType] = useState(SortType.NONE);
   const [isReversed, setIsReversed] = useState(false);
   const goodies = getReorderedGoods(goodsFromServer, sortType, isReversed);
+
+  const reset = () => {
+    setIsReversed(false);
+    setSortType(SortType.NONE);
+  };
 
   return (
     <div className="section content">
@@ -51,9 +65,9 @@ export const App: React.FC = () => {
         <button
           type="button"
           className={classNames("button is-info", {
-            "is-light": sortType !== "ALPHABET",
+            "is-light": sortType !== SortType.ALPHABET,
           })}
-          onClick={() => setSortType("ALPHABET")}
+          onClick={() => setSortType(SortType.ALPHABET)}
         >
           Sort alphabetically
         </button>
@@ -61,9 +75,9 @@ export const App: React.FC = () => {
         <button
           type="button"
           className={classNames("button is-success", {
-            "is-light": sortType !== "LENGTH",
+            "is-light": sortType !== SortType.LENGTH,
           })}
-          onClick={() => setSortType("LENGTH")}
+          onClick={() => setSortType(SortType.LENGTH)}
         >
           Sort by length
         </button>
@@ -77,14 +91,11 @@ export const App: React.FC = () => {
         >
           Reverse
         </button>
-        {sortType !== "NONE" || isReversed ? (
+        {sortType !== SortType.NONE || isReversed ? (
           <button
             type="button"
             className="button is-danger is-light"
-            onClick={() => {
-              setIsReversed(false);
-              setSortType("NONE");
-            }}
+            onClick={reset}
           >
             Reset
           </button>
