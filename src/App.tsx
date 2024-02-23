a10fd231f678dd2bd0bc085ed13e96040cc30954
volodymyr <volodiayurovych@gmail.com>
2024-02-23 19:39:39 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
-import classNames from 'classnames';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -16,110 +15,89 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
-  enum SortType {
-    Alphabetically = 'sortAlphabet',
-    Lengthly = 'sortLength',
-    None = '',
+enum SortType {
+  Alphabetically = 'sortAlphabet',
+  Lengthly = 'sortLength',
+  None = '',
+}
+
+function action(
+  initialValue: string[],
+  howToSotr: SortType,
+  reverse: boolean,
+): string[] {
+  const sortedGoods = [...initialValue];
+
+  switch (howToSotr) {
+    case SortType.Alphabetically:
+      sortedGoods.sort((el1, el2) => el1.localeCompare(el2));
+      break;
+    case SortType.Lengthly:
+      sortedGoods.sort((el1, el2) => el1.length - el2.length);
+      break;
+    default:
+      break;
   }
 
+  if (reverse) {
+    sortedGoods.reverse();
+  }
+
+  return sortedGoods;
+}
+
+export const App: React.FC = () => {
   const [sortField, setSortField] = useState<SortType>(SortType.None);
   const [isReversed, setIsReversed] = useState(false);
 
-  function action(
-    initialValue: string[],
-    howToSotr: SortType,
-    reverse: boolean,
-  ): string[] {
-    const sortedGoods = [...initialValue];
-
-    switch (howToSotr) {
-      case SortType.Alphabetically:
-        sortedGoods.sort((el1, el2) => el1.localeCompare(el2));
-        break;
-      case SortType.Lengthly:
-        sortedGoods.sort((el1, el2) => el1.length - el2.length);
-        break;
-      default:
-        break;
-    }
-
-    if (reverse) {
-      sortedGoods.reverse();
-    }
-
-    return sortedGoods;
-  }
-
   const toRender = action(goodsFromServer, sortField, isReversed);
-
-  const alphabetClass = classNames({
-    button: true,
-    'is-info': true,
-    'is-light': !(sortField === SortType.Alphabetically),
-  });
-
-  const lengthClass = classNames({
-    button: true,
-    'is-success': true,
-    'is-light': !(sortField === SortType.Lengthly),
-  });
-
-  const reverseClass = classNames({
-    button: true,
-    'is-warning': true,
-    'is-light': !isReversed,
-  });
-
-  const resetClass = classNames({
-    button: true,
-    'is-danger': true,
-    'is-light': true,
-    hiden: !(sortField !== SortType.None || isReversed === true),
-  });
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
-          onClick={() => setSortField(SortType.Alphabetically)}
+          onClick={(): void => setSortField(SortType.Alphabetically)}
           type="button"
-          className={alphabetClass}
+          className={`button is-info ${sortField !== SortType.Alphabetically && 'is-light'}`}
         >
           Sort alphabetically
         </button>
 
         <button
-          onClick={() => setSortField(SortType.Lengthly)}
+          onClick={(): void => setSortField(SortType.Lengthly)}
           type="button"
-          className={lengthClass}
+          className={`button is-success ${sortField !== SortType.Lengthly && 'is-light'}`}
         >
           Sort by length
         </button>
 
         <button
-          onClick={() => setIsReversed(!isReversed)}
+          onClick={(): void => setIsReversed(!isReversed)}
           type="button"
-          className={reverseClass}
+          className={`button is-warning ${!isReversed && 'is-light'}`}
         >
           Reverse
         </button>
 
-        <button
-          onClick={() => {
-            setSortField(SortType.None);
-            setIsReversed(false);
-          }}
-          type="button"
-          className={resetClass}
-        >
-          Reset
-        </button>
+        {(sortField !== SortType.None || isReversed) && (
+          <button
+            onClick={(): void => {
+              setSortField(SortType.None);
+              setIsReversed(false);
+            }}
+            type="button"
+            className="button is-danger is-light"
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         {toRender.map(item => (
-          <li key={item}>{item}</li>
+          <li data-cy="Good" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </div>
