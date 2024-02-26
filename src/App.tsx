15431c53afffbcda968f183f3db41f2ce3bb0ed4
Dmytro Chadov <Dmytrochd@gmail.com>
2024-02-26 07:16:58 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { GoodList } from './GoodList/GoodList';
+
+function getPreparedGoods(goods: string[], sortField: string) {
+  const preparedGoods = [...goods];
+
+  if (sortField) {
+    preparedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case 'name':
+          return good1.localeCompare(good2);
+
+        case 'length':
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return preparedGoods;
+}
 
 export const goodsFromServer = [
   'Dumplings',
@@ -16,36 +39,66 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
+  const [sortField, setSortField] = useState('');
+  const [isAscending, setIsAscending] = useState(true);
+
+  const toggleSortOrder = () => {
+    setIsAscending(!isAscending);
+  };
+
+  function reset() {
+    setSortField('');
+    setIsAscending(true);
+  }
+
+  const sortedGoods = getPreparedGoods(goodsFromServer, sortField);
+  const visibleGoods = isAscending ? sortedGoods : sortedGoods.reverse();
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          onClick={() => setSortField('name')}
+          className={cn('button', 'is-info', {
+            'is-light': sortField !== 'name',
+          })}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          onClick={() => setSortField('length')}
+          className={cn('button', 'is-success', {
+            'is-light': sortField !== 'length',
+          })}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': isAscending,
+          })}
+          onClick={toggleSortOrder}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {(sortField !== '' || !isAscending) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodList goods={visibleGoods} />
     </div>
   );
 };
---
+++ src/GoodList/GoodList.tsx
@@ -0,0 +1,13 @@
+import { GoodCard } from '../GoodCard/GoodCard';
+
+type Props = {
+  goods: string[];
+};
+
+export const GoodList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <GoodCard good={good} />
+    ))}
+  </ul>
+);
