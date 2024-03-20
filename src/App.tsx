6a6b9e301020ec69e68a9da111b2b18bb27392a5
Mariia Dzhafarova <grinchak.1997@gmail.com>
2024-03-20 14:39:10 +0200

    improve solution

--- src/App.tsx
+++ src/App.tsx
@@ -28,21 +28,33 @@ interface FilterParams {
 }
 
 function getPreperedGoods({ sortField, isReversed }: FilterParams): string[] {
-  const preperedGoods = [...goodsFromServer];
+  let preperedGoods = [...goodsFromServer];
+
+  const sortGoodsByAlphabet = (): void => {
+    preperedGoods = preperedGoods.sort((good1, good2) =>
+      good1.localeCompare(good2),
+    );
+  };
+
+  const sortGoodsByLength = (): void => {
+    preperedGoods = preperedGoods.sort(
+      (good1, good2) => good1.length - good2.length,
+    );
+  };
 
   if (sortField) {
-    preperedGoods.sort((good1, good2) => {
-      switch (sortField) {
-        case SortType.Alphabet:
-          return good1.localeCompare(good2);
+    switch (sortField) {
+      case SortType.Alphabet:
+        sortGoodsByAlphabet();
+        break;
 
-        case SortType.Length:
-          return good1.length - good2.length;
+      case SortType.Length:
+        sortGoodsByLength();
+        break;
 
-        default:
-          return 0;
-      }
-    });
+      default:
+        return preperedGoods;
+    }
   }
 
   if (isReversed) {
@@ -57,7 +69,7 @@ export const App = () => {
   const [isReversed, setIsReversed] = useState(false);
 
   const visibleGoods = getPreperedGoods({ sortField, isReversed });
-  const sortedOrReversedList = sortField || isReversed;
+  const isResetActive = sortField || isReversed;
 
   const resetSortHandler = (): void => {
     setsortField(SortType.Default);
@@ -97,7 +109,7 @@ export const App = () => {
           Reverse
         </button>
 
-        {sortedOrReversedList && (
+        {isResetActive && (
           <button
             onClick={resetSortHandler}
             type="button"
@@ -118,129 +130,3 @@ export const App = () => {
     </div>
   );
 };
-
-// import React, { useState } from 'react';
-// import cn from 'classnames';
-// import 'bulma/css/bulma.css';
-// import './App.scss';
-
-// export const goodsFromServer = [
-//   'Dumplings',
-//   'Carrot',
-//   'Eggs',
-//   'Ice cream',
-//   'Apple',
-//   'Bread',
-//   'Fish',
-//   'Honey',
-//   'Jam',
-//   'Garlic',
-// ];
-
-// enum SortBy {
-//   Default = '',
-//   Alphabetically = 'alphabetically',
-//   Length = 'length',
-// }
-
-// interface FilterParams {
-//   sortBy: SortBy;
-//   isReversed: boolean;
-// }
-
-// function getPreparedGoods(
-//   goods: string[],
-//   { sortBy, isReversed }: FilterParams,
-// ): string[] {
-//   const currentVisibleGoods = [...goods];
-
-//   if (sortBy) {
-//     currentVisibleGoods.sort((good1, good2) => {
-//       if (sortBy === SortBy.Alphabetically) {
-//         return good1.localeCompare(good2);
-//       }
-
-//       if (sortBy === SortBy.Length) {
-//         return good1.length - good2.length;
-//       }
-
-//       return 0;
-//     });
-//   }
-
-//   if (isReversed) {
-//     currentVisibleGoods.reverse();
-//   }
-
-//   return currentVisibleGoods;
-// }
-
-// export const App: React.FC = () => {
-//   const [sortByState, setSortByState] = useState(SortBy.Default);
-//   const [reversedState, setReversedState] = useState(false);
-//   const visibleGoods = getPreparedGoods(goodsFromServer, {
-//     sortBy: sortByState,
-//     isReversed: reversedState,
-//   });
-
-//   const resetHandler = (): void => {
-//     setSortByState(SortBy.Default);
-//     setReversedState(false);
-//   };
-
-//   const isGoodsSorted = sortByState || reversedState;
-
-//   return (
-//     <div className="section content">
-//       <div className="buttons">
-//         <button
-//           type="button"
-//           className={cn('button', 'is-info', {
-//             'is-light': sortByState !== SortBy.Alphabetically,
-//           })}
-//           onClick={() => setSortByState(SortBy.Alphabetically)}
-//         >
-//           Sort alphabetically
-//         </button>
-
-//         <button
-//           type="button"
-//           className={cn('button', 'is-success', {
-//             'is-light': sortByState !== SortBy.Length,
-//           })}
-//           onClick={() => setSortByState(SortBy.Length)}
-//         >
-//           Sort by length
-//         </button>
-
-//         <button
-//           type="button"
-//           className={cn('button', 'is-warning', {
-//             'is-light': !reversedState,
-//           })}
-//           onClick={() => setReversedState(!reversedState)}
-//         >
-//           Reverse
-//         </button>
-
-//         {isGoodsSorted && (
-//           <button
-//             type="button"
-//             className="button is-danger is-light"
-//             onClick={resetHandler}
-//           >
-//             Reset
-//           </button>
-//         )}
-//       </div>
-
-//       <ul>
-//         <ul>
-//           {visibleGoods.map((good: string) => (
-//             <li data-cy="Good">{good}</li>
-//           ))}
-//         </ul>
-//       </ul>
-//     </div>
-//   );
-// };
