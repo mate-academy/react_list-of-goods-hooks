385deb230ba7f14ee3b7069ecf8a7454dcfba3f8
verandetta <verandetta@yahoo.com>
2024-03-27 11:42:23 +0200

    add task solution

---
+++ src/types/FilterParams.ts
@@ -0,0 +1,6 @@
+import { SortType } from './SortType';
+
+export interface FilterParams {
+  sortfield: SortType;
+  isReversed: boolean;
+}
---
+++ src/types/SortType.ts
@@ -0,0 +1,5 @@
+export enum SortType {
+  SORT_FIELD_ALPHABET = 'ALPHABET',
+  SORT_FIELD_LENGTH = 'LENGTH',
+  SORT_FIELD_DEFAULT = '',
+}
