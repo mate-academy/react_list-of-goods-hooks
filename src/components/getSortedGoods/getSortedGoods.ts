8f6020d780608543273612e0bd4707bd39913356
Tetiana Veremchuk <tetianaverem@gmail.com>
2024-04-29 19:42:54 +0300

    add task solution

---
+++ src/components/getSortedGoods/getSortedGoods.ts
@@ -0,0 +1,28 @@
+import { SortType, SortedGoods } from '../../App';
+
+export const getSortedGoods = (
+  goods: string[],
+  { sortField, goodsReverse }: SortedGoods,
+) => {
+  let prepearedGoods: string[] = [...goods];
+
+  if (sortField) {
+    prepearedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SortType.sortAlphabet:
+          return good1.localeCompare(good2);
+        case SortType.sortLength:
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (goodsReverse) {
+    prepearedGoods = prepearedGoods.reverse();
+  }
+
+  return prepearedGoods;
+};
