f80f70643b0b02f9791bddb3cd3869270b031781
eduvinagre <eduvinagre@gmail.com>
2024-04-29 16:23:59 -0300

    adding task corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,58 +15,73 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortingType {
+  Alphabetical = 'alphabetical',
+  Length = 'length',
+  NotSorted = 'notsorted',
+}
+
+const sortingFunctions = {
+  [SortingType.Alphabetical]: (a: string, b: string) => a.localeCompare(b),
+  [SortingType.Length]: (a: string, b: string) => a.length - b.length,
+  [SortingType.NotSorted]: false,
+};
+
+const handleSorting = (
+  sortedState: SortingType,
+  reverseList: boolean,
+  setList: (list: string[]) => void,
+) => {
+  const sortedList = [...goodsFromServer];
+  const sortingFunction = sortingFunctions[sortedState];
+
+  if (typeof sortingFunction === 'function') {
+    sortedList.sort(sortingFunction);
+  }
+
+  if (reverseList) {
+    sortedList.reverse();
+  }
+
+  setList(sortedList);
+};
+
 const App: React.FC = () => {
   const [list, setList] = useState<string[]>(goodsFromServer);
   const [reverseList, setReverseList] = useState<boolean>(false);
-  const [sortedState, setSortedState] = useState<number>(0);
+  const [sortedState, setSortedState] = useState<SortingType>(
+    SortingType.NotSorted,
+  );
 
   const reset = () => {
     setList(goodsFromServer);
     setReverseList(false);
-    setSortedState(0);
+    setSortedState(SortingType.NotSorted);
   };
 
-  const sortedGoods = (goods: string[]) => {
-    setSortedState(1);
-    const result = [...goods].sort((a, b) => a.localeCompare(b));
-
-    if (reverseList) {
-      return result.reverse();
-    }
-
-    return result;
-  };
-
-  const sortedGoodsLength = (goods: string[]) => {
-    setSortedState(2);
-    if (reverseList) {
-      return [...goods].sort((a, b) => b.length - a.length);
-    }
-
-    return [...goods].sort((a, b) => a.length - b.length);
-  };
-
-  const reversedGoods = (goods: string[]) => {
-    setReverseList(!reverseList);
-
-    return [...goods].reverse();
-  };
+  useEffect(() => {
+    handleSorting(sortedState, reverseList, setList);
+  }, [sortedState, reverseList]);
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className={`button is-info ${sortedState === 1 ? '' : 'is-light'}`}
-          onClick={() => setList(sortedGoods(list))}
+          className={`button is-info ${sortedState === SortingType.Alphabetical ? '' : 'is-light'}`}
+          onClick={() => {
+            setSortedState(SortingType.Alphabetical);
+          }}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className={`button is-success ${sortedState === 2 ? '' : 'is-light'}`}
-          onClick={() => setList(sortedGoodsLength(list))}
+          className={`button is-success ${sortedState === SortingType.Length ? '' : 'is-light'}`}
+          onClick={() => {
+            setSortedState(SortingType.Length);
+          }}
         >
           Sort by length
         </button>
@@ -74,16 +89,16 @@ const App: React.FC = () => {
         <button
           type="button"
           className={`button is-warning ${reverseList ? '' : 'is-light'}`}
-          onClick={() => setList(reversedGoods(list))}
+          onClick={() => setReverseList(item => !item)}
         >
           Reverse
         </button>
 
-        {(sortedState !== 0 || reverseList) && (
+        {(sortedState !== SortingType.NotSorted || reverseList) && (
           <button
             type="button"
             className="button is-danger is-light"
-            onClick={() => reset()}
+            onClick={reset}
           >
             Reset
           </button>
