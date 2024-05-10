8c9197cb7458f258a3d3b49c69fbe574d39d287f
mfcastilho <fredericocastilho@hotmail.com>
2024-05-10 02:37:12 -0300

    feat: add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import React, { useState } from 'react';
+import { GoodList } from './GoodList/index';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -16,36 +17,82 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
+  const [sortedState, setSortedState] = useState<number>(0);
+  const [reverseList, setReverseList] = useState<boolean>(false);
+  const [changedGoods, setChangedGoods] = useState<string[]>(goodsFromServer);
+
+  const handleAlphaSort = (): void => {
+    if (sortedState !== 1) {
+      setSortedState(1);
+
+      if (reverseList) {
+        setChangedGoods(
+          [...changedGoods].sort((a, b) => a.localeCompare(b)).reverse(),
+        );
+      } else {
+        setChangedGoods([...changedGoods].sort((a, b) => a.localeCompare(b)));
+      }
+    }
+  };
+
+  const handleLengthSort = (): void => {
+    if (sortedState !== 2) {
+      setSortedState(2);
+
+      if (reverseList) {
+        setChangedGoods([...changedGoods].sort((a, b) => b.length - a.length));
+      } else {
+        setChangedGoods([...changedGoods].sort((a, b) => a.length - b.length));
+      }
+    }
+  };
+
+  const handleReverse = (): void => {
+    setReverseList(!reverseList);
+    setChangedGoods([...changedGoods].reverse());
+  };
+
+  const handleReset = (): void => {
+    setChangedGoods(goodsFromServer);
+    setSortedState(0);
+    setReverseList(false);
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={`button is-info ${sortedState === 1 ? '' : 'is-light'}`}
+          onClick={handleAlphaSort}
+        >
           Sort alphabetically
         </button>
-
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={`button is-success ${sortedState === 2 ? '' : 'is-light'}`}
+          onClick={handleLengthSort}
+        >
           Sort by length
         </button>
-
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={`button is-warning ${reverseList ? '' : 'is-light'}`}
+          onClick={handleReverse}
+        >
           Reverse
         </button>
-
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {(sortedState !== 0 || reverseList) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        )}
       </div>
-
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodList goodsFromServer={changedGoods} />
     </div>
   );
 };
