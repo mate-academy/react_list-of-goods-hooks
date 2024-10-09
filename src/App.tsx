f956cdb03c89fefa1d505844104d20821277357d
Oleh Senkiv <o.senkiv.v@gmail.com>
2024-10-09 18:33:50 +0300

    Implemented React list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
+import { GoodsList } from './GoodsList/GoodsList';
+import { Good } from './types/Good';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,37 +18,83 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Alphabetically,
+  ByLenght,
+  Revers,
+  Reset,
+}
+
 export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+
+  const showGoodsList = () => {
+    setGoods([...goodsFromServer]);
+  };
+
+  const handleClick = (sortType: SortType) => {
+    switch (sortType) {
+      case SortType.Alphabetically:
+        return setGoods([...goods].sort((a, b) => a.localeCompare(b)));
+      case SortType.ByLenght:
+        return setGoods([...goods].sort((a, b) => a.length - b.length));
+      case SortType.Revers:
+        return setGoods([...goods].reverse());
+      case SortType.Reset:
+        return setGoods([...goodsFromServer]);
+
+      default:
+        return 0;
+    }
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={classNames('button', 'button--start', {
+            hover: goods.length !== 0,
+          })}
+          onClick={showGoodsList}
+        >
+          Select
+        </button>
+
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={() => handleClick(SortType.Alphabetically)}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => handleClick(SortType.ByLenght)}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => handleClick(SortType.Revers)}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => handleClick(SortType.Reset)}
+        >
           Reset
         </button>
       </div>
 
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodsList goods={[...goods]} />
     </div>
   );
 };
