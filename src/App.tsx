d64c9fcd9087e200181acd90af47f513bab07427
Elisabeth Polska <polskaelizaveta@gmail.com>
2025-04-25 17:22:35 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -15,37 +15,109 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
-  return (
-    <div className="section content">
-      <div className="buttons">
-        <button type="button" className="button is-info is-light">
-          Sort alphabetically
-        </button>
+enum SortType {
+  Default = 'default',
+  Alphabetically = 'alphabetically',
+  Length = 'length',
+  Reversed = 'reversed',
+}
 
-        <button type="button" className="button is-success is-light">
-          Sort by length
-        </button>
-
-        <button type="button" className="button is-warning is-light">
-          Reverse
-        </button>
-
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
-      </div>
-
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
-    </div>
-  );
+type State = {
+  goods: string[];
+  sortOrder: SortType;
+  reset: boolean;
 };
+
+export class App extends React.Component<{}, State> {
+  originalGoods: string[] = [...goodsFromServer];
+
+  state: State = {
+    goods: [...goodsFromServer],
+    sortOrder: SortType.Default,
+    reset: false,
+  };
+
+  sortAlphabetically = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort(),
+      sortOrder: SortType.Alphabetically,
+      reset: true,
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => a.length - b.length),
+      sortOrder: SortType.Length,
+      reset: true,
+    }));
+  };
+
+  reverseOrder = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+      sortOrder: SortType.Reversed,
+      reset: true,
+    }));
+  };
+
+  resetOrder = () => {
+    this.setState({
+      goods: [...this.originalGoods],
+      sortOrder: SortType.Default,
+      reset: false,
+    });
+  };
+
+  render() {
+    const { goods, sortOrder, reset } = this.state;
+
+    return (
+      <div className="section content">
+        <div className="buttons">
+          <button
+            type="button"
+            className={`button is-info ${sortOrder === SortType.Alphabetically ? '' : 'is-light'}`}
+            onClick={this.sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            className={`button is-success ${sortOrder === SortType.Length ? '' : 'is-light'}`}
+            onClick={this.sortByLength}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className={`button is-warning ${sortOrder === SortType.Reversed ? '' : 'is-light'}`}
+            onClick={this.reverseOrder}
+          >
+            Reverse
+          </button>
+
+          {reset && (
+            <button
+              type="button"
+              className="button is-danger"
+              onClick={this.resetOrder}
+            >
+              Reset
+            </button>
+          )}
+        </div>
+
+        <ul>
+          {goods.map(good => (
+            <li key={good} data-cy="Good">
+              {good}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
