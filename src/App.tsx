837b80385668dceaf2e0233d2074908bc8fa8676
Yura Soroka <ykotov18@gmail.com>
2025-05-28 17:16:04 +0300

    solutio2

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,10 @@ export const goodsFromServer = [
 enum SortBy {
   ALPHABET = 'alphabetical',
   LENGTH = 'length',
+  DEFAULT = '',
 }
 
-const preparedGoods = (goods: string[], sortBy: SortBy, isReverse: boolean) => {
+const preparedGoods = (goods: string[], sortBy: SortBy | null, isReverse: boolean) => {
   const copyGoods = [...goods];
 
   switch (sortBy) {
@@ -45,75 +46,75 @@ const preparedGoods = (goods: string[], sortBy: SortBy, isReverse: boolean) => {
 
 export const App: React.FC = () => {
   const [goodsType, setGoodsType] = useState<SortBy | null>(null);
-    const [isReverse, setIsReversed] = useState(false);
+  const [isReverse, setIsReversed] = useState(false);
 
-    const visibleGoods = preparedGoods(goodsFromServer, goodsType, isReverse);
+  const visibleGoods = preparedGoods(goodsFromServer, goodsType, isReverse);
 
-    const handleSortedByAlph = () => {
-      setGoodsType(SortBy.ALPHABET);
-    };
+  const handleSortedByAlph = () => {
+    setGoodsType(SortBy.ALPHABET);
+  };
 
-    const handleSortedByLength = () => {
-      setGoodsType(SortBy.LENGTH);
-    };
+  const handleSortedByLength = () => {
+    setGoodsType(SortBy.LENGTH);
+  };
 
-    const handleReversed = () => {
-      setIsReversed(!isReverse);
-    };
+  const handleReversed = () => {
+    setIsReversed(!isReverse);
+  };
 
-    const handleReset = () => {
-      setGoodsType(null);
-      setIsReversed(false);
-    };
+  const handleReset = () => {
+    setGoodsType(null);
+    setIsReversed(false);
+  };
 
-    return (
-      <div className="section content">
-        <div className="buttons">
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${goodsType === SortBy.ALPHABET ? '' : 'is-light'}`}
+          onClick={handleSortedByAlph}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className={`button is-success ${goodsType === SortBy.LENGTH ? '' : 'is-light'}`}
+          onClick={handleSortedByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-warning ${isReverse ? '' : 'is-light'}`}
+          onClick={handleReversed}
+        >
+          Reverse
+        </button>
+
+        {(goodsType !== null || isReverse === true) && (
           <button
             type="button"
-            className={`button is-info ${goodsType === SortBy.ALPHABET ? '' : 'is-light'}`}
-            onClick={handleSortedByAlph}
+            className="button is-danger is-light"
+            onClick={handleReset}
           >
-            Sort alphabetically
+            Reset
           </button>
-
-          <button
-            type="button"
-            className={`button is-success ${goodsType === SortBy.LENGTH ? '' : 'is-light'}`}
-            onClick={handleSortedByLength}
-          >
-            Sort by length
-          </button>
-
-          <button
-            type="button"
-            className={`button is-warning ${isReverse ? '' : 'is-light'}`}
-            onClick={handleReversed}
-          >
-            Reverse
-          </button>
-
-          {(goodsType !== null || isReverse === true) && (
-            <button
-              type="button"
-              className="button is-danger is-light"
-              onClick={handleReset}
-            >
-              Reset
-            </button>
-          )}
-        </div>
-
-        <ul>
-          {visibleGoods.map(good => (
-            <li key={good} data-cy="Good">
-              {good}
-            </li>
-          ))}
-          {visibleGoods.length < goodsFromServer.length && (
-            <li data-cy="Good">...</li>
-          )}
-        </ul>
+        )}
       </div>
-    );
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+        {visibleGoods.length < goodsFromServer.length && (
+          <li data-cy="Good">...</li>
+        )}
+      </ul>
+    </div>
+  );
 };
