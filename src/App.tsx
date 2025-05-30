e684be8f5d2989f2024d7c3bd146fa60a6bb9090
Ivan <ivancvnts@hotmail.com>
2025-05-29 19:49:38 -0600

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -102,13 +102,11 @@ export const App: React.FC = () => {
       </div>
 
       <ul>
-        <ul>
-          {sortedGoods.map(good => (
-            <li key={good} data-cy="Good">
-              {good}
-            </li>
-          ))}
-        </ul>
+        {sortedGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
