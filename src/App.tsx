2e29b147840f4f660d91d8794f043eae9db9885e
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-09-30 14:41:38 +0200

    saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
-export const goodsFromServer = [
+enum SortType {
+  alphabet = 'Sort alphabetically',
+  length = 'Sort by length',
+  reverse = 'Reverse',
+  reset = 'Reset',
+}
+
+export const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -16,36 +23,128 @@ export const goodsFromServer = [
 ];
 
 export const App: React.FC = () => {
+  const [goods, setGoods] = useState<string[]>([...goodsFromServer]);
+  const [count, setCount] = useState<string>('1');
+  const [count1, setCount1] = useState<string>('1');
+  const [count2, setCount2] = useState<boolean>(true);
+  const [count3, setCount3] = useState<string>('1');
+
+  const sortGoods = (type: SortType) => {
+    let sorted = [...goods];
+
+    switch (type) {
+      case SortType.alphabet:
+        if (!count2) {
+          sorted.sort((a, b) => b.localeCompare(a));
+        } else {
+          sorted.sort((a, b) => a.localeCompare(b));
+        }
+
+        break;
+
+      case SortType.length:
+        if (!count2) {
+          sorted.sort((a, b) => b.length - a.length);
+        } else {
+          sorted.sort((a, b) => a.length - b.length);
+        }
+
+        break;
+
+      case SortType.reverse:
+        sorted.reverse();
+        break;
+
+      default:
+        sorted = [...goodsFromServer];
+    }
+
+    setGoods(sorted);
+  };
+
+  const handleFunction = (value: SortType) => {
+    sortGoods(value);
+
+    if (value === SortType.alphabet) {
+      setCount('0');
+      setCount1('1');
+      setCount3('0');
+    } else if (value === SortType.length) {
+      setCount('1');
+      setCount1('0');
+      setCount3('0');
+    } else if (value === SortType.reverse) {
+      setCount2(prev => {
+        const newVal = !prev;
+
+        sortGoods(value);
+
+        return newVal;
+      });
+      setCount3('0');
+    } else if (value === SortType.reset) {
+      setCount3('1');
+      setCount('1');
+      setCount1('1');
+      setCount2(true);
+    }
+  };
+
+  const getColor = (value: SortType) => {
+    if (value === SortType.alphabet) {
+      return `is-info    ${count === '1' ? 'is-light' : ''}`;
+    }
+
+    if (value === SortType.length) {
+      return `is-success ${count1 === '1' ? 'is-light' : ''}`;
+    }
+
+    if (value === SortType.reverse) {
+      return `is-warning ${count2 ? 'is-light' : ''}`;
+    }
+
+    if (value === SortType.reset) {
+      return `is-danger  ${count3 === '0' ? 'is-light' : ''}`;
+    }
+
+    return '';
+  };
+
   return (
-    <div className="section content">
-      <div className="buttons">
-        <button type="button" className="button is-info is-light">
-          Sort alphabetically
-        </button>
+    <>
+      <div className="section content">
+        <div className="buttons">
+          {Object.values(SortType).map(value => {
+            if (
+              value === SortType.reset &&
+              goods.every((item, i) => item === goodsFromServer[i])
+            ) {
+              return null;
+            }
 
-        <button type="button" className="button is-success is-light">
-          Sort by length
-        </button>
+            return (
+              <button
+                key={value}
+                type="button"
+                className={`button ${getColor(value)}`}
+                onClick={() => {
+                  handleFunction(value);
+                }}
+              >
+                {value}
+              </button>
+            );
+          })}
+        </div>
 
-        <button type="button" className="button is-warning is-light">
-          Reverse
-        </button>
-
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
-      </div>
-
-      <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {goods.map(good => (
+            <li key={good} data-cy="Good">
+              {good}
+            </li>
+          ))}
         </ul>
-      </ul>
-    </div>
+      </div>
+    </>
   );
 };
