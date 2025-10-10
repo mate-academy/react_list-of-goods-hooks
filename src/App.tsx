cbc640508777cd71255755c314dded240ae8b7d8
diego69-foda <diego.bkuhn@gmail.com>
2025-10-10 16:40:58 -0300

    Refactor App component for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import 'bulma/css/bulma.css';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 
 export const goodsFromServer = [
@@ -15,32 +15,58 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
+export const App: React.FC = () => { 
+  const [goods, setGoods] = useState(goodsFromServer);
+  
+  const sortAlphabetically = useCallback(() => {
+    setGoods(
+      [...goods].sort((a, b) => a.localeCompare(b)),
+    );
+  }, [goods]);
+
+  const sortByLength = useCallback(() => {
+    setGoods(
+      [...goods].sort((a, b) => a.length - b.length),
+    );
+  }, [goods]);
+
+  const reverseGoods = useCallback(() => {
+    setGoods(
+      [...goods].reverse(),
+    );
+  }, [goods]);
+
+  const resetGoods = useCallback(() => {
+    setGoods(goodsFromServer);
+  }, []);
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={sortAlphabetically}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button type="button" className="button is-success is-light" onClick={sortByLength}>
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button type="button" className="button is-warning is-light" onClick={reverseGoods}>
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
+        <button type="button" className="button is-danger is-light" onClick={resetGoods}>
           Reset
         </button>
       </div>
-
+      
       <ul>
-        {goodsFromServer.map((good: string) => (
-          <li key={good}>
-            {good}
-          </li>
+        {goods.map((good: string) => (
+          <li key={good}>{good}</li>
         ))}
       </ul>
     </div>
