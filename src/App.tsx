47635c32b5dfef71b8c10cc9dc5f0faf08cb4140
Nazar <nazar.shevchuk.121@gmail.com>
2026-01-25 18:50:33 +0100

    sol1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,36 +15,142 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App: React.FC = () => {
+// export const App: React.FC = () => {
+//   return (
+//     <div className="section content">
+//       <div className="buttons">
+//         <button type="button" className="button is-info is-light">
+//           Sort alphabetically
+//         </button>
+
+//         <button type="button" className="button is-success is-light">
+//           Sort by length
+//         </button>
+
+//         <button type="button" className="button is-warning is-light">
+//           Reverse
+//         </button>
+
+//         <button type="button" className="button is-danger is-light">
+//           Reset
+//         </button>
+//       </div>
+
+//       <ul>
+//         <ul>
+//           <li data-cy="Good">Dumplings</li>
+//           <li data-cy="Good">Carrot</li>
+//           <li data-cy="Good">Eggs</li>
+//           <li data-cy="Good">Ice cream</li>
+//           <li data-cy="Good">Apple</li>
+//           <li data-cy="Good">...</li>
+//         </ul>
+//       </ul>
+//     </div>
+//   );
+// };
+
+function areArraysEqual(arr1: string[], arr2: string[]) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((el, i) => el === arr2[i]);
+}
+
+export const App = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [reverse, setReverse] = useState(false);
+  const [sortMode, setSortMode] = useState('none');
+
+  const reverseGoods = () => {
+    setGoods([...goods].reverse());
+    setReverse(!reverse);
+  };
+
+  const resetGoods = () => {
+    setGoods([...goodsFromServer]);
+    setSortMode('');
+    setReverse(false);
+  };
+
+  const alphabetGoods = [...goodsFromServer].sort((a, b) => a.localeCompare(b));
+
+  const sortGoods = (mode: string) => {
+    setSortMode(mode);
+    if (mode === 'alphabet') {
+      const alphabetGoodsNew = alphabetGoods;
+
+      if (reverse === true) {
+        setGoods([...alphabetGoodsNew].reverse());
+      } else {
+        setGoods(alphabetGoodsNew);
+      }
+    }
+
+    if (mode === 'length') {
+      const lengthGoods = [...goodsFromServer].sort(
+        (a, b) => a.length - b.length,
+      );
+
+      if (reverse === true) {
+        setGoods([...lengthGoods].reverse());
+      } else {
+        setGoods(lengthGoods);
+      }
+    }
+  };
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={`button is-info ${sortMode === 'alphabet' ? '' : 'is-light'}`}
+          onClick={() => {
+            sortGoods('alphabet');
+          }}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={`button is-success ${sortMode === 'length' ? '' : 'is-light'}`}
+          onClick={() => {
+            sortGoods('length');
+          }}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={`button is-warning ${!reverse ? 'is-light' : ''}`}
+          onClick={reverseGoods}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {!areArraysEqual(goods, goodsFromServer) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={resetGoods}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {[...goods].map(good => {
+          return (
+            <li data-cy="Good" key={good}>
+              {good}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
