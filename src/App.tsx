9f9fcec301cf06eafd6df69d2295188158dd3ab0
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-02-10 19:43:16 +0100

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import classNames from 'classnames';
+import { GoodsList } from './components/goodsList/GoodsList';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -15,37 +17,96 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+export enum SortOptions {
+  Alphabetically = 'alphabetically',
+  Length = 'length',
+}
+
+interface SortParams {
+  sortStatus: SortOptions | '';
+  reverse: boolean;
+}
+
+function sortGoods(goods: string[], { sortStatus, reverse }: SortParams) {
+  const preparedGoods = [...goods];
+
+  if (sortStatus !== '') {
+    preparedGoods.sort((good1, good2) => {
+      switch (sortStatus) {
+        case SortOptions.Alphabetically:
+          return good1.localeCompare(good2);
+        case SortOptions.Length:
+          return good1.length - good2.length;
+      }
+    });
+  }
+
+  if (reverse) {
+    preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
+
 export const App: React.FC = () => {
+  const [reverse, setReverse] = useState(false);
+  const [sortStatus, setSortOptions] = useState<SortOptions | ''>('');
+  const visibleGood = sortGoods(goodsFromServer, { sortStatus, reverse });
+  const reset = goodsFromServer.some(
+    (value, index) => value !== visibleGood[index],
+  );
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={classNames('button', 'is-info', {
+            'is-light': sortStatus !== SortOptions.Alphabetically,
+          })}
+          onClick={() => {
+            setSortOptions(SortOptions.Alphabetically);
+          }}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={classNames('button', 'is-success', {
+            'is-light': sortStatus !== SortOptions.Length,
+          })}
+          onClick={() => {
+            setSortOptions(SortOptions.Length);
+          }}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={classNames('button is-warning', { 'is-light': !reverse })}
+          onClick={() => {
+            setReverse(!reverse);
+          }}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {reset && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setReverse(false);
+              setSortOptions('');
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
-
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
+      <GoodsList goods={visibleGood} />
     </div>
   );
 };
---
+++ src/components/goodsList/GoodsList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type Props = {
+  goods: string[];
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul>
+      <ul>
+        {goods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
+    </ul>
+  );
+};
