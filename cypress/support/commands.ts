21bce97bd0657e905ccdc75b22492fa5d50c6911
Goat <vova.cher@yahoo.com>
2026-06-25 14:55:43 +0300

    Solution

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -47,8 +47,8 @@ declare global {
   }
 }
 
-Cypress.Commands.add('getByDataCy', selector => {
-  cy.get(`[data-cy="${selector}"]`);
+Cypress.Commands.add('getByDataCy', (value: string) => {
+  return cy.get(`[data-cy="${value}"]`);
 });
 
 Cypress.Commands.add(
