4d81e0bc50f2ab94ed92d0c563a36429f04200ff
Tetiana <skutar26@gmail.com>
2026-07-23 19:23:08 +0200

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ export const App: React.FC = () => {
               setIsReversed(false);
             }}
             type="button"
-            className={'button·is-danger·is-light'}
+            className={'button is-danger is-light'}
           >
             Reset
           </button>
